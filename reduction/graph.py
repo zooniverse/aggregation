338d0830f6075fb6ea101c1734a311b9c9afea66
Edward Paget <edward@cassetteta.pe>
2014-05-13 17:13:32 -0500

    Make graph a little more OO

--- reduction/graph.py
+++ reduction/graph.py
@@ -1,70 +1,115 @@
 import networkx as nx
+class Node(object):
+    def __init__(self, id, p):
+        self.p = p
+        self.id = id
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __eq__(self, test):
+        return isinstance(test, type(self)) and hash(self) == hash(test)
+
+    def __str__(self):
+        return str(self.id) + " " + str(self.p)
+
+class Task(Node):
+    def __init__(self, t_id, p=0):
+        super().__init__(t_id, p)
+
+class Worker(Node):
+    def __init__(self, w_id):
+        super().__init__(w_id, 0)
+
+class GoldTask(Task):
+    def __init__(self, gt_id):
+        super().__init__(gt_id, p=1)
+
+class NodeSet(object):
+    def __init__(self, graph, type):
+        self._nodes = set()
+        self._parent = graph
+        self._type = type
+
+    def add_node(self, node):
+        self._nodes.add(node)
+
+    def contains(self, n):
+        return self._type(n) in self._nodes
+
+    def neighbors_and_edges(self, id, condition):
+        return [(self._parent[id][n],self._parent.node[n]) for n 
+                    in self._parent.neighbors_iter(id)
+                    if condition(n)]
+
+    def __iter__(self, condition=lambda n: True):
+        for n in self._nodes:
+            attr = self._parent.node[n]
+            neighbors = self.neighbors_and_edges(n, condition)
+            degree = self._parent.degree(n)
+            yield n,degree,attr,neighbors
+
+class WorkerSet(NodeSet):
+    def cond(self, n):
+        return type(n) is Task
+    
+    def __iter__(self):
+        for n,degree,attr,neighbors in super().__iter__(condition=self.cond):
+            neighbor_degree = len(neighbors)
+            excluded = degree - neighbor_degree
+            degree = degree,excluded,neighbor_degree
+            yield n,degree,attr,neighbors
 
 class Graph(object):
-    class NodeSet(object):
-        def __init__(self, graph):
-            self._nodes = []
-            self._parent = graph
-
-        def add_node(self, node):
-            self._nodes.append(node)
-
-        def neighbors_and_edges(self, id, condition):
-            return [(self._parent[id][n],self._parent.node[n]) for n 
-                        in self._parent.neighbors_iter(id)
-                        if condition(n)]
-
-        def __iter__(self, condition=lambda n: True):
-            for n in self._nodes:
-                attr = self._parent.node[n]
-                neighbors = self.neighbors_and_edges(n, condition)
-                degree = self._parent.degree(n)
-                yield n,degree,attr,neighbors
-
-    class WorkerSet(NodeSet):
-        def __iter__(self):
-            cond = lambda n: self._parent.node[n]['kind'] == 'task'
-            for n,degree,attr,neighbors in super().__iter__(condition=cond):
-                neighbor_degree = len(neighbors)
-                excluded = degree - neighbor_degree
-                degree = degree,excluded,neighbor_degree
-                yield n,degree,attr,neighbors
-
     def __init__(self):
         self._graph = nx.Graph()
-        self._workers = self.WorkerSet(self._graph)
-        self._tasks = self.NodeSet(self._graph)
-        self._gold_tasks = self.NodeSet(self._graph)
+        self._workers = WorkerSet(self._graph, Worker)
+        self._tasks = NodeSet(self._graph, Task)
+        self._gold_tasks = NodeSet(self._graph, GoldTask)
 
-    def _add_node(self, id, type, attributes):
-        self._graph.add_node(id, kind=type)
+    def _add_node(self, n, attributes):
+        self._graph.add_node(n)
         for k,v in attributes.items():
-            if k != 'kind':
-                self._graph[id][k] = v 
+            self._graph[n][k] = v 
+        if type(n) is Worker:
+            self._workers.add_node(n)
+        elif type(n) is Task:
+            self._tasks.add_node(n)
+        elif type(n) is GoldTask:
+            self._gold_tasks.add_node(n)
 
     def add_worker(self, worker_id, attributes={}):
-        self._add_node(worker_id, 'worker', attributes={})
-        self._workers.add_node(worker_id)
+        self._add_node(Worker(worker_id), attributes={})
 
     def add_task(self, task_id, attributes={}):
-        self._add_node(task_id, 'task', attributes)
-        self._tasks.add_node(task_id)
+        self._add_node(Task(task_id), attributes)
 
     def add_gold_task(self, gold_task_id, attributes={}):
-        self._add_node(gold_task_id, 'gold_task', attributes)
-        self._gold_tasks.add_node(gold_task_id)
+        self._add_node(GoldTask(gold_task_id), attributes)
 
     def add_answer(self, worker_id, task_id, answer, attributes={}):
-        self._graph.add_edge(worker_id, task_id, answer=answer)
+        if self._tasks.contains(task_id):
+            task = Task(task_id)
+        elif self._gold_tasks.contains(task_id):
+            task = GoldTask(task_id)
+        else:
+            task = None
+
+        worker = Worker(worker_id)
+
+        print(task)
+        self._graph.add_edge(worker, task, answer=answer)
+
         for k,v in attributes.items():
             if k != 'answer':
-                self._graph.edge[worker_id][task_id][k] = v 
+                self._graph.edge[worker][task][k] = v 
 
     def workers(self):
-        return self._workers.__iter__()
+        return iter(self._workers)
 
     def tasks(self):
-        return self._tasks.__iter__()
+        return iter(self._tasks)
 
     def gold_tasks(self):
-        return self._gold_tasks.__iter__()
+        return iter(self._gold_tasks)
+
