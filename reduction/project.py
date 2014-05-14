a59d0f370ed326aeef8ae13cf59e633de7bf6ff4
Edward Paget <edward@cassetteta.pe>
2014-05-14 17:20:25 -0500

    Start implementing generic projects

--- reduction/project.py
+++ reduction/project.py
@@ -6,15 +6,27 @@ class Project(object):
     def __init__(self, config):
         self.config = config
 
+
 class BinaryQuestionProject(Project):
 
     def __init__(self, config):
         super().__init__(config)
 
+
 class OneDimensionalMarkingProject(BinaryQuestionProject):
 
-    def __init__(self, config):
-        super().__init__(config)
+    def __init__(self, **kwargs):
+        self.length = kwargs['x']
+
+    def __call__(self, db, algo):
+        g = self.build_graph(db)
+        return algo(g)
+
+    def build_graph(self, db, algo):
+        users = db.users()
+        subjects = db.users()
+        annotations = db.users()
+
 
 class TwoDimensionalMarkingProject(BinaryQuestionProject):
 
@@ -29,5 +41,3 @@ class TwoDimensionalMarkingProject(BinaryQuestionProject):
     def build_graph(self, db):
         g = Graph()
         return g
-
-
