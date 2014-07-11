ed4ec656b3fa8ddd4da946cfe877c2e6d35f6f35
ggdhines <ggdhines@gmail.com>
2014-07-11 17:15:45 +0100

    gah

--- experimental/hive/majorityVote/mapper1.py
+++ experimental/hive/majorityVote/mapper1.py
@@ -5,11 +5,21 @@ import sys
 # input comes from STDIN (standard input)
 for i,line in enumerate(sys.stdin):
     try:
-        (subject_zooniverse_id,user_name,species,capture_event_id) = line.strip().split()
+        (subject_zooniverse_id,user_name,species,capture_event_id) = line.strip().split('\t')
     except ValueError:
         #assume that this
+        sys.stderr.write(line)
+        sys.stderr.write((str(line.strip().split('\t'))))
+        raise
+
+    if (user_name == "user_name") or (user_name == "\"user_name\""):
+        continue
 
     if (capture_event_id == "\"tutorial\"") or (capture_event_id == "tutorial"):
+        continue
+
+    if ":" in species:
+        #badly formed data
         continue
 
     if species == "":
--- experimental/hive/weightedVote/consistencyReducer.py
+++ experimental/hive/weightedVote/consistencyReducer.py
@@ -6,7 +6,10 @@ consistencies = None
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
-    subject_zooniverse_id, user_name, classification, voteDistributon = line.split("\t")
+    subject_zooniverse_id, user_name, classification, voteDistributon = line.strip().split("\t")
+
+    if (classification[0] == "\"") and (classification[-1] == "\""):
+        classification = classification[1:-1]
 
     if current_zooniverse_id != subject_zooniverse_id:
         if current_zooniverse_id is not None:
@@ -18,11 +21,21 @@ for line in sys.stdin:
     #convert voteDistribution
     consistency = 0
     numCategories = 0.
-    found = False
+
     for v in voteDistributon.split(" "):
         numCategories += 1
+        found = False
 
-        c,f = v.split(":")
+        try:
+            v = v.strip()
+            c,f = v.split(":")
+        except ValueError:
+            sys.stderr.write(line+"\n")
+            sys.stderr.write(voteDistributon+"\n")
+            sys.stderr.write(v+"\n")
+            sys.stderr.write(str(v.split(":"))+"\n")
+            raise
+
         f = float(f)
         if c == classification:
             consistency += f
@@ -30,7 +43,12 @@ for line in sys.stdin:
         else:
             consistency += (1-f)
 
-    assert found
+        if not found:
+            sys.stderr.write(line+"\n")
+            sys.stderr.write(classification+"\n")
+            sys.stderr.write(voteDistributon+"\n")
+            sys.stderr.write(c + "\n")
+            assert found
     consistency = consistency/numCategories
 
     print subject_zooniverse_id + "\t" + user_name + "\t%.3f" % consistency
