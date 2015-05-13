dba80a5e9fcf80c7873fd3371fa36e68d4a4346d
Greg <ggdhines@gmail.com>
2015-05-13 20:07:43 +0100

    blog update

--- blog/plankton.py
+++ blog/plankton.py
@@ -1,12 +1,13 @@
 __author__ = 'ggdhines'
 from ouroboros_api import MarkingProject
-from agglomerative2 import Agglomerative
-from classification import MajorityVote,AllOrNothing
+from agglomerative import Agglomerative
+from classification import MajorityVote,IBCC
 import numpy
+import matplotlib.pyplot as plt
 
-class FloatingForests(MarkingProject):
-    def __init__(self,date="2015-05-06",pickle_directory="/tmp/"):
-        MarkingProject.__init__(self, "plankton", date,experts=["yshish"],pickle_directory=pickle_directory)
+class PlanktonPortal(MarkingProject):
+    def __init__(self,date="2015-05-08"):
+        MarkingProject.__init__(self, "plankton", date,experts=["yshish"])
 
     def __annotations_to_markings__(self,annotations):
         """
@@ -30,20 +31,38 @@ class FloatingForests(MarkingProject):
                 species = ann["species"]
             except KeyError:
                 continue
+
+            if species == "":
+                continue
             markings.append(((x,y),species))
 
         # assert False
         return markings
 
-project = FloatingForests()
-project.__top_users__()
-agglomerative = Agglomerative(project)
-classifier = AllOrNothing(project,agglomerative)
+project = PlanktonPortal()
+# project.__top_users__()
+project.__set_gold_standard__(max_subjects=500)
+
+clustering = Agglomerative(project)
+classifier = IBCC(project,clustering)
 
 for subject_id in project.gold_standard_subjects:
-    agglomerative.__cluster_subject__(subject_id,gold_standard =True)
+    print subject_id
+    project.__store_markings__(subject_id,max_users=20)
+    clustering.__fit__(subject_id)
+    clustering.__fit__(subject_id,gold_standard=True)
 
-print classifier.__fit__(project.gold_standard_subjects,gold_standard=True)
+
+candidates,results = classifier.__classify__(project.gold_standard_subjects,gold_standard=False)
+errors,percentage = project.__evaluate__(candidates,results,clustering)
+
+sorted_percentage = sorted(list(set(percentage)))
+c = zip(errors,percentage)
+
+avg_error = [numpy.mean([e for e,p in c if p >= s]) for s in sorted_percentage]
+coverage = [sum([1 for e,p in c if p >= s])/float(len(c)) for s in sorted_percentage]
+plt.plot(coverage,avg_error)
+plt.show()
 # project.__set_gold_standard__(limit=100)
 #
 # agglomerative = Agglomerative(project)
