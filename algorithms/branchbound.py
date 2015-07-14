a5e624715acf90989c2d86ddd8b5096cfc247830
Greg <ggdhines@gmail.com>
2015-07-14 12:08:07 +0100

    cleaning unused files

--- algorithms/branchbound.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'greg'
-from transcription import LongestCommonSubsequence
-
-
-class BranchBound(LongestCommonSubsequence):
-    def __init__(self):
-        LongestCommonSubsequence.__init__(self)
--- algorithms/dynamic_programming.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'greg'
-from transcription import LongestCommonSubsequence
-
-
-class DynamicProgramming(LongestCommonSubsequence):
-    def __init__(self):
-        LongestCommonSubsequence.__init__(self)
