83a055d8781fe4efea31d9a59dddc1131b33163a
Edward Paget <edward@cassetteta.pe>
2015-08-13 17:18:19 -0500

    Update api and remove s3 client requirement

--- engine/jobs.py
+++ engine/jobs.py
@@ -1,23 +1,33 @@
-import boto3
 import os
 import requests
 import json
 from aggregation_api import AggregationAPI
 
-def aggregate(project_id, href, bucket, key, metadata):
-    s3 = boto3.resource('s3')
-    project = AggregationAPI(project_id)
+def aggregate(project_id, token, href, metadata, environment="development"):
+    project = AggregationAPI(project_id, environment=environment)
     project.__aggregate__()
     tarpath = project.__csv_output__(compress=True)
+    response = send_uploading(metadata)
+    url = response.json()["media"][0]["src"]
     with open(tarpath, 'rb') as tarball:
-        s3.Bucket(bucket).put_objject(Key=key, Body=tarball)
+        requests.put(url, data=tarball)
     os.remove(tarpath)
-    metadata['state'] = 'ready'
+    send_finished(metadata)
+
+def send_uploading(metadata):
+    metadata['state'] = 'uploading'
     body = { 'media': { 'metadata': metadata } }
+    send_request(body)
+
+def send_finished(metadata):
+    metadata['state'] = 'finished'
+    body = { 'media': { 'metadata': metadata } }
+    send_request(body)
+
+def send_request(body):
     headers = { 'Accept': 'application/vnd.api+json; version=1',
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer '+str(token) }
-
     requests.put(href, headers=headers, data=json.dumps(body))
 
 
--- engine/web_api.py
+++ engine/web_api.py
@@ -8,24 +8,36 @@ import os
 import json
 
 app = Flask(__name__)
-q = Queue(connection=configure_redis(os.getenv('FLASK_ENV', 'production')))
+env = os.getenv('FLASK_ENV', 'production')
+q = Queue(connection=configure_redis(env))
+apis = {
+    'development': "http://"+str(os.getenv('HOST_IP', '172.17.42.1'))+":3000",
+    'stagin': "https://panoptes-staging.zooniverse.org",
+    'production': "https://panoptes.zooniverse.org"
+
+}
+
+api_root = apis[env]
 
 @app.route('/',methods=['POST'])
 def start_aggregation():
     try:
         body = request.get_json()
         project = body['project_id']
-        bucket = body['bucket']
-        key = body['key']
-        href = body['href']
+        href = body['media_href']
         metadata = body['metadata']
-        q.enqueue(aggregate, project, href, bucket, key, metadata)
+        token = body['token']
+        q.enqueue(aggregate, project, token, api_root+"/api"+href, metadata)
         resp = make_response(json.dumps({'queued': True}), 200)
         resp.headers['Content-Type'] = 'application/json'
         return resp
     except KeyError:
         resp = make_response(json.dumps({error: [{messages: "Missing Required Key"}]}), 422)
+        resp.headers['Content-Type'] = 'application/json'
         return resp
+    except:
+        app.logger.info("TEST")
+        app.logger.info(sys.exc_info()[0])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
