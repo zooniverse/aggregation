a8cc6a3e0df7b8fad63969a92044ef423dcc103a
Edward Paget <edward@cassetteta.pe>
2015-08-14 11:55:01 -0500

    Allow cassandra and api connections to switch
    ...base on environments. Add docker-compose file to run required
    services for local tests

--- engine/jobs.py
+++ engine/jobs.py
@@ -7,29 +7,38 @@ def aggregate(project_id, token, href, metadata, environment):
     project = AggregationAPI(project_id, environment=environment)
     project.__aggregate__()
     tarpath = project.__csv_output__(compress=True)
-    response = send_uploading(metadata)
+    response = send_uploading(metadata, token, href)
     url = response.json()["media"][0]["src"]
     with open(tarpath, 'rb') as tarball:
         requests.put(url, data=tarball)
     os.remove(tarpath)
-    send_finished(metadata)
+    send_finished(metadata, token, href)
 
-def send_uploading(metadata):
+def get_etag(href, token):
+    response = requests.get(href, headers=headers(token), params={'admin': True})
+    return response.headers['ETag']
+
+def send_uploading(metadata, token, href):
     metadata['state'] = 'uploading'
     body = { 'media': { 'metadata': metadata } }
-    send_request(body)
+    return send_request(body, token, href)
 
-def send_finished(metadata):
+def send_finished(metadata, token, href):
     metadata['state'] = 'finished'
     body = { 'media': { 'metadata': metadata } }
-    send_request(body)
+    return send_request(body, token, href)
 
-def send_request(body):
-    body['admin'] = true
+def headers(token, etag=False):
     headers = { 'Accept': 'application/vnd.api+json; version=1',
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer '+str(token) }
-    requests.put(href, headers=headers, data=json.dumps(body))
+    if etag:
+        headers['If-Match'] = get_etag(etag, token)
+    return headers
+
+def send_request(body, token, href):
+    body['admin'] = True
+    return requests.put(href, headers=headers(token, etag=href), data=json.dumps(body))
 
 
 
