c0ae7d576b48c2c5fbe7ad0f570ceb2673953f3b
Greg <ggdhines@gmail.com>
2015-08-17 23:14:46 +0100

    adding in dbscan

--- algorithms/old_weather/t.py
+++ algorithms/old_weather/t.py
@@ -6,8 +6,9 @@ import matplotlib.cbook as cbook
 import math
 import numpy as np
 import cv2
+from sklearn.cluster import DBSCAN
 
-img = cv2.imread("/home/greg/Dropbox/066e48f5-812c-4b5f-ab04-df6c35f50393.jpeg")
+# img = cv2.imread("/home/greg/Dropbox/066e48f5-812c-4b5f-ab04-df6c35f50393.jpeg")
 
 with open('old_weather.json') as data_file:
     data = json.load(data_file)
@@ -17,8 +18,8 @@ scale_y = 1.45
 x_offset = 20
 y_offset = 2
 
-image_file = cbook.get_sample_data("/home/greg/Dropbox/066e48f5-812c-4b5f-ab04-df6c35f50393.jpeg")
-image = plt.imread(image_file)
+# image_file = cbook.get_sample_data("/home/ggdhines/Dropbox/066e48f5-812c-4b5f-ab04-df6c35f50393.jpeg")
+image = plt.imread("/home/ggdhines/Dropbox/066e48f5-812c-4b5f-ab04-df6c35f50393.jpeg")
 # print type(image[0])
 # assert False
 
@@ -78,6 +79,8 @@ for box in data["classifications"][0]["annotations"]:
 
     plt.show()
 
+    pts = []
+
     for y_i in range(y,y2+1):
     # for x_i in range(x,x2+1):
         # for y_i in range(y,y2+1):
@@ -85,7 +88,45 @@ for box in data["classifications"][0]["annotations"]:
             p = image[y_i][x_i]
             if math.sqrt(sum([(a-b)**2 for (a,b) in zip(base,p)])) > 50:
                 plt.plot(x_i,-y_i,'o',color="black")
+                pts.append((x_i,-y_i))
 
     plt.show()
+    pts = np.asarray(pts)
+    db = DBSCAN(eps=1, min_samples=2).fit(pts)
+    labels = db.labels_
+
+    # Number of clusters in labels, ignoring noise if present.
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+    unique_labels = set(labels)
+    colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+
+    clusters = []
+
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = 'k'
+
+        class_member_mask = (labels == k)
+
+        xy = pts[class_member_mask]
+        clusters.append(xy[:])
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,markeredgecolor='k', markersize=14)
+
+    plt.show()
+
+    for c in clusters:
+        x,y = zip(*c)
+
+        if min((max(x) - min(x)),(max(y) - min(y))) == 0:
+            continue
+        largest = max((max(x) - min(x)),(max(y) - min(y)))
+        print 28/float(largest)
+
+        for i in range(28):
+            for j in range(28):
+                
+    break
 
 # plt.show()
