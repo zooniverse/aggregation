a6f159c0e3fb80611b0e55984c43c9489828af18
Edward Paget <edward@cassetteta.pe>
2015-08-18 10:36:21 -0500

    Make sure debug is off don't catch 500 errors

--- engine/web_api.py
+++ engine/web_api.py
@@ -35,10 +35,5 @@ def start_aggregation():
         resp.headers['Content-Type'] = 'application/json'
         return resp
 
-@app.errorhandler(500)
-def handle_error(error):
-    print error
-    return "Internal Server Error"
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=False)
