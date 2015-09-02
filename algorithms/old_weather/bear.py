ba853358742b13643a3753a2622079159aacd2d8
Greg Hines <greg@zooniverse.org>
2015-09-02 20:34:22 +0100

    trying to get ocr working

--- algorithms/old_weather/bear.py
+++ algorithms/old_weather/bear.py
@@ -3,27 +3,30 @@ import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
 import math
 import numpy
-import cPickle
-import os
-from neural_network import Network,load_data_wrapper
-import cv2
 from scipy import spatial
+from PIL import Image
+import pytesseract
+
+
+
+
+
 
 start_x = 585.5
 end_x = 3265.5
 horizontal_lines = [(1271.5,1288.5),(1368.5,1384),(1424,1440),(1481.5,1497),(1537.5,1552),(1593,1607.5),(1649,1663),(1705.5,1718.5),(1762.5,1775.5),(1819,1830.5),(1875,1886),(1931,1942),(1987,1998)]
 
-vertical_lines = [586,778.5,882,948,1049.5,1049.5,1117,1293,1456.5,1557.5,1758.5,1852,1959,2058.5,2157.5,3225,2559.5,2731,2855.5,2959,3095,3266]
+vertical_lines = [586,778.5,882,948,1049.5,1117,1293,1456.5,1557.5,1758.5,1852,1959,2058.5,2157.5,2325,2559.5,2731,2855.5,2959,3095,3266]
 start_y = 1271.5
 end_y = 1998
 
-def intersection(((y1,y2),(x1,x2)),x_):
-    m = (y2-y1)/float(x2-x1)
-    b = y2 - m*x2
-
-    y_intersect = m*x_+b
-
-    return x_,y_intersect
+# def intersection(((y1,y2),(x1,x2)),x_):
+#     m = (y2-y1)/float(x2-x1)
+#     b = y2 - m*x2
+#
+#     y_intersect = m*x_+b
+#
+#     return x_,y_intersect
 
 def line_segment((x1,y1),(x2,y2)):
     assert x1 < x2
@@ -43,12 +46,12 @@ fig = plt.figure()
 axes = fig.add_subplot(1, 1, 1)
 im = axes.imshow(image)
 
-for y_l in horizontal_lines:
-    for x in vertical_lines:
-        x_intersect,y_intersect = intersection((y_l,(start_x,end_x)),x)
-        plt.plot(x_intersect,y_intersect,"o",color="red")
-
-plt.show()
+# for y_l in horizontal_lines:
+#     for x in vertical_lines:
+#         x_intersect,y_intersect = intersection((y_l,(start_x,end_x)),x)
+#         plt.plot(x_intersect,y_intersect,"o",color="red")
+#
+# plt.show()
 
 ref1 = [218,219,213]
 
@@ -64,17 +67,15 @@ for y in range(1271,2000):
         if dist1 > 25:
             X_l.append(x)
             Y_l.append(y)
-plt.plot(X_l,Y_l,"o",color="blue")
-
-plt.ylim((2000,1271))
-plt.xlim((585,3265))
+# plt.plot(X_l,Y_l,"o",color="blue")
+#
+# plt.ylim((2000,1271))
+# plt.xlim((585,3265))
 
 
 tree = spatial.KDTree(zip(X_l,Y_l))
 
 # corner = 2858,1934
-
-
 
 def corner_correction(corner):
     X_l2 = []
@@ -102,42 +103,91 @@ def corner_correction(corner):
 
     return x1,y1
 
-tl_x,tl_y = corner_correction((1118,1593))
-# plt.plot(1118,1593,"o",color="green")
-# plt.plot(x1,y1,"o",color="yellow")
-bl_x,bl_y = corner_correction((1118,1650))
-# plt.plot(1118,1650,"o",color="green")
-# plt.plot(x1,y1,"o",color="yellow")
-tr_x,tr_y = corner_correction((1294,1593))
-# plt.plot(1294,1593,"o",color="green")
-# plt.plot(x1,y1,"o",color="yellow")
-br_x,br_y = corner_correction((1294,1650))
-# plt.plot(1294,1650,"o",color="green")
-# plt.plot(x1,y1,"o",color="yellow")
 
-# print image
-
-# s = image.shape[0]*image.shape[1]
-# print s
-# print image.shape[0],image.shape[1]
-# image.reshape((s,1,3))
-#
-#
-# print image[0:10]
+def deter(a,b,c,d):
+    return a*d - c*b
 
 
-rows = numpy.array([[tl_y+i for j in range(tr_x-tl_x+1)] for i in range(bl_y-tl_y+1)])
-print rows
-columns = numpy.array([[tl_x+j for j in range(tr_x-tl_x+1)] for i in range(bl_y-tl_y+1)])
-print columns
-subimage = image[rows,columns]
+def intersection(h_l,v_l):
+    x1 = start_x
+    y1 = h_l[0]
 
-for i in range(subimage.shape[0]):
-    for j in range(subimage.shape[1]):
-        dist1 = math.sqrt(sum([(a-b)**2 for (a,b) in zip(subimage[i][j],ref1)]))
-        if dist1 < 25:
-            subimage[i][j] = [255,255,255]
+    x2 = end_x
+    y2 = h_l[1]
 
-cv2.imwrite("/home/greg/output.png", image[rows,columns])
-# plt.show()
+    x3 = v_l
+    y3 = start_y
+
+    x4 = v_l
+    y4 = end_y
+
+    d1 = deter(x1,y1,x2,y2)
+    d2 = deter(x1,1,x2,1)
+    d3 = deter(x3,y3,x4,y4)
+    d4 = deter(x3,1,x4,1)
+
+    D1 = deter(d1,d2,d3,d4)
+
+    d5 = deter(y1,1,y2,1)
+    d6 = deter(y3,1,y4,1)
+
+    D2 = deter(d1,d5,d3,d6)
+
+    d7 = deter(x3,1,x4,1)
+    d8 = deter(y3,1,y4,1)
+
+    D3 = deter(d2,d5,d7,d8)
+
+    intersect_x = int(D1/D3)
+    intersect_y = int(D2/D3)
+
+    return intersect_x,intersect_y
+
+plt.show()
+
+for h_index,h_l in enumerate(horizontal_lines[:-1]):
+    for v_index,v_l in enumerate(vertical_lines[:-1]):
+        ul_x,ul_y = intersection(h_l,v_l)
+        ll_x,ll_y = intersection(horizontal_lines[h_index+1],v_l)
+        ur_x,ur_y = intersection(h_l,vertical_lines[v_index+1])
+        lr_x,lr_y = intersection(horizontal_lines[h_index+1],vertical_lines[v_index+1])
+
+        x_0 = min(ul_x,ll_x)
+        print h_l,v_l
+        print horizontal_lines[h_index+1],vertical_lines[v_index+1]
+        print "==----"
+        print (ul_x,ll_x)
+        x_1 = max(ur_x,lr_x)
+        print (ur_x,lr_x)
+
+        y_0 = min(ul_y,ur_y)
+        print (ul_y,ur_y)
+        y_1 = max(ll_y,lr_y)
+        print (ll_y,lr_y)
+        print
+
+        rows = numpy.array([[y_0+i for j in range(x_1-x_0+1)] for i in range(y_1-y_0+1)])
+        columns = numpy.array([[x_0+j for j in range(x_1-x_0+1)] for i in range(y_1-y_0+1)])
+        subimage = image[rows,columns]
+
+        # for i in range(subimage.shape[0]):
+        #     for j in range(subimage.shape[1]):
+        #         dist1 = math.sqrt(sum([(a-b)**2 for (a,b) in zip(subimage[i][j],ref1)]))
+        #         if dist1 < 25:
+        #             subimage[i][j] = [255,255,255]
+
+        r = Image.fromarray(subimage)
+        r.save("/home/greg/temp.jpg")
+        # r = Image.open("/home/greg/temp.jpg")
+        # r.save("/home/greg/temp.pdf","PDF", Quality = 100)
+        # print(pytesseract.image_to_string(Image.open("/home/greg/output.pdf")))
+
+        fig = plt.figure()
+        axes = fig.add_subplot(1, 1, 1)
+        im = axes.imshow(subimage)
+        plt.show()
+
+        # plt.plot(intersect_x,intersect_y,"o",color="yellow")
+
+
 
