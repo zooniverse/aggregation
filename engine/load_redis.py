be44e6cf7f05d9dbf6ca27af1be49eac9bce7096
Greg Hines <greg@zooniverse.org>
2015-09-21 15:17:45 +0100

    see previous

--- engine/load_redis.py
+++ engine/load_redis.py
@@ -1,1 +1,10 @@
-__author__ = 'greg'
+from redis import Redis
+import yaml
+
+def configure_redis(environment):
+    with open("config/redis.yml") as redis_file:
+        options = yaml.load(redis_file)
+        options_for_env = options[environment]
+        return Redis(host=options_for_env['host'],
+                     port=options_for_env['port'],
+                     db=options_for_env['db'])
