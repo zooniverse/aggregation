c2ae376c233877d0de61a88d9fc6e4218800f4ac
Greg Hines <greg@zooniverse.org>
2015-09-24 14:09:29 +0100

    added rollbar support

--- engine/jobs.py
+++ engine/jobs.py
@@ -1,18 +1,35 @@
 import os
 import requests
 import json
-from aggregation_api import AggregationAPI
+from aggregation_api import base_directory,AggregationAPI
+import yaml
+import rollbar
 
 def aggregate(project_id, token, href, metadata, environment):
-    project = AggregationAPI(project_id, environment=environment)
-    project.__aggregate__()
-    tarpath = project.__csv_output__(compress=True)
-    response = send_uploading(metadata, token, href)
-    url = response.json()["media"][0]["src"]
-    with open(tarpath, 'rb') as tarball:
-        requests.put(url, headers={'Content-Type': 'application/x-gzip'}, data=tarball)
-    os.remove(tarpath)
-    send_finished(metadata, token, href)
+    try:
+        panoptes_file = open("config/aggregation.yml","rb")
+    except IOError:
+        panoptes_file = open(base_directory+"/Databases/aggregation.yml","rb")
+    api_details = yaml.load(panoptes_file)
+
+    rollbar_token = None
+    if "rollbar" in api_details[environment]:
+        rollbar_token = api_details[environment]["rollbar"]
+        rollbar.init(rollbar_token,"production")
+
+    try:
+        project = AggregationAPI(project_id, environment=environment)
+        project.__aggregate__()
+        tarpath = project.__csv_output__(compress=True)
+        response = send_uploading(metadata, token, href)
+        url = response.json()["media"][0]["src"]
+        with open(tarpath, 'rb') as tarball:
+            requests.put(url, headers={'Content-Type': 'application/x-gzip'}, data=tarball)
+        os.remove(tarpath)
+        send_finished(metadata, token, href)
+    except:
+        if rollbar_token is not None:
+            rollbar.report_exc_info()
 
 def get_etag(href, token):
     response = requests.get(href, headers=headers(token), params={'admin': True})
