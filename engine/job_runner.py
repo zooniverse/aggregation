9ee98404b883cd7feea75a4a01138b6cf59afac9
Campbell Allen <campbell.allen@gmail.com>
2015-09-25 18:16:14 +0100

    ensure we can load the jobs runner
    couldn't find the setup_rollbar function and the rollbar.contrib.rq module wasn't setup for exception handling.

--- engine/job_runner.py
+++ engine/job_runner.py
@@ -5,23 +5,21 @@ from load_redis import configure_redis
 from aggregation_api import base_directory
 import yaml
 import rollbar
-
-def setup_rollbar(env):
-    try:
-        panoptes_file = open("config/aggregation.yml","rb")
-    except IOError:
-        panoptes_file = open(base_directory+"/Databases/aggregation.yml","rb")
-
-    api_details = yaml.load(panoptes_file)
-    rollbar_token = api_details[env]["rollbar"]
-    rollbar.init(rollbar_token, env, handler='blocking')
+from rollbar.contrib.rq import exception_handler
 
 listen = ['high', 'default', 'low']
 env = os.getenv('FLASK_ENV', 'production')
 
-conn = configure_redis(env)
-setup_rollbar(env)
+try:
+    panoptes_file = open("config/aggregation.yml","rb")
+except IOError:
+    panoptes_file = open(base_directory+"/Databases/aggregation.yml","rb")
 
+api_details = yaml.load(panoptes_file)
+rollbar_token = api_details[env]["rollbar"]
+rollbar.init(rollbar_token, env, handler='blocking')
+
+conn = configure_redis(env)
 with Connection(conn):
     worker = Worker(map(Queue, listen))
     worker.push_exc_handler(rollbar.contrib.rq.exception_handler)
