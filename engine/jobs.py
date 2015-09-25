4f1d4de268f761de85b5d2199e6b99cb02d0d554
Greg Hines <greg@zooniverse.org>
2015-09-25 12:32:21 +0100

    just trying to get rollbar to report

--- engine/jobs.py
+++ engine/jobs.py
@@ -19,27 +19,28 @@ def aggregate(project_id, token, href, metadata, environment):
 
     print rollbar_token
     rollbar.init(rollbar_token,environment)
-    rollbar.report_message("step1","info")
+    print "hello"
+    rollbar.report_message("step5","info")
 
-    try:
-        with AggregationAPI(project_id, environment=environment) as project:
-            project.__migrate__()
-            project.__aggregate__()
-            rollbar.report_message("step2","info")
-
-            with CsvOut(project) as writer:
-                rollbar.report_message("step3","info")
-                tarpath = writer.__write_out__(compress=True)
-                response = send_uploading(metadata, token, href)
-                url = response.json()["media"][0]["src"]
-                with open(tarpath, 'rb') as tarball:
-                    requests.put(url, headers={'Content-Type': 'application/x-gzip'}, data=tarball)
-                os.remove(tarpath)
-                send_finished(metadata, token, href)
-                rollbar.report_message("step4","info")
-    except Exception, err:
-        print traceback.format_exc()
-        rollbar.report_exc_info()
+    # try:
+    #     with AggregationAPI(project_id, environment=environment) as project:
+    #         project.__migrate__()
+    #         project.__aggregate__()
+    #         rollbar.report_message("step6","info")
+    #
+    #         with CsvOut(project) as writer:
+    #             rollbar.report_message("step3","info")
+    #             tarpath = writer.__write_out__(compress=True)
+    #             response = send_uploading(metadata, token, href)
+    #             url = response.json()["media"][0]["src"]
+    #             with open(tarpath, 'rb') as tarball:
+    #                 requests.put(url, headers={'Content-Type': 'application/x-gzip'}, data=tarball)
+    #             os.remove(tarpath)
+    #             send_finished(metadata, token, href)
+    #             rollbar.report_message("step4","info")
+    # except Exception, err:
+    #     print traceback.format_exc()
+    #     rollbar.report_exc_info()
 
 
 
