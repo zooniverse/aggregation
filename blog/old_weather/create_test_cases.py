6b3ab95379e6f6ded9951aff7d794e4065896c4a
Greg Hines <greg@zooniverse.org>
2015-11-09 18:36:06 +0000

    working on generalizing to multiple cells

--- blog/old_weather/create_test_cases.py
+++ blog/old_weather/create_test_cases.py
@@ -4,11 +4,16 @@ import numpy as np
 import Image
 from extract_digits import extract
 
-image_directory = "/home/ggdhines/Databases/old_weather/images/"
-template_image = "Bear-AG-29-1939-0175.JPG"
+image_directory = "/home/ggdhines/Databases/old_weather/test_cases/"
+template_image = "Bear-AG-29-1941-0414.JPG"
+
+
+cell_columns = [(496,698),(698,805),(805,874),(1051,1234),(1405,1508),(1508,1719),(1719,1816),(1816,1927),(1927,2032),(2032,2134),(2733,2863),(2863,2971),(2971,3133)]
+cell_rows = [(1267,1370),(1370,1428),(1428,1488),(1488,1547),(1547,1606),(1606,1665),(1665,1723),(1723,1781),(1781,1840),(1840,1899),(1899,1957),(1957,2016)]
 
 for f_count,f_name in enumerate(os.listdir(image_directory)):
-    if f_count == 20:
+    f_name = "Bear-AG-29-1941-0557.JPG"
+    if f_count == 1:
         break
     if f_name.endswith(".JPG"):
         print image_directory+f_name
@@ -23,13 +28,17 @@ for f_count,f_name in enumerate(os.listdir(image_directory)):
         # # break
         # # print type(image)
         # # sub_image = image[162:238,274:326,:]
-        r = range(1103,1281)
-        c = range(1436,1491)
-        sub_image = image[np.ix_(c, r)]
 
-        img = Image.fromarray(sub_image, 'RGB')
-        img.save('/home/ggdhines/a.png')
+        for column_lb,column_ub in cell_columns:
+            for row_lb,row_ub in cell_rows:
+                r = range(row_lb,row_ub+1)
+                c = range(column_lb,column_lb+1)
+
+                sub_image = image[np.ix_(c, r)]
+
+                img = Image.fromarray(sub_image, 'RGB')
+                img.save('/home/ggdhines/a.png')
 
 
-        extract(sub_image)
-        raw_input("enter something")
+                extract(sub_image)
+                raw_input("enter something")
