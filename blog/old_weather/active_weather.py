9aafc7e443879d02d8828bae282feb4ce24b7953
Greg <ggdhines@gmail.com>
2015-12-01 23:58:55 +0000

    honestly can't remember

--- blog/old_weather/active_weather.py
+++ blog/old_weather/active_weather.py
@@ -1,5 +1,5 @@
-import matplotlib
-matplotlib.use('WXAgg')
+# import matplotlib
+# matplotlib.use('WXAgg')
 from skimage.transform import (hough_line, hough_line_peaks,
                                probabilistic_hough_line)
 from skimage.feature import canny
@@ -116,10 +116,13 @@ gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,25,150,apertureSize = 3)
 # cv2.imwrite('/home/ggdhines/1.jpg',edges)
 
-lines = probabilistic_hough_line(edges, threshold=5, line_length=50,line_gap=0)
+lines = probabilistic_hough_line(edges, threshold=8, line_length=8,line_gap=1)
 fig, ax1 = plt.subplots(1, 1)
 fig.set_size_inches(52,78)
-ax1.imshow(image)
+ax1.imshow(edges)
+
+plt.savefig("/home/ggdhines/Databases/example.jpg",bbox_inches='tight', pad_inches=0,dpi=72)
+assert False
 
 horiz_list = []
 horiz_intercepts = []
@@ -127,10 +130,15 @@ horiz_intercepts = []
 vert_list = []
 vert_intercepts = []
 
+
+
 big_lower_x = 559
 big_upper_x = 3245
 big_lower_y = 1292
 big_upper_y = 2014
+
+horiz_lines = []
+vert_lines = []
 
 for line in lines:
     p0, p1 = line
@@ -145,12 +153,14 @@ for line in lines:
 
             m = (Y[0]-Y[1])/float(X[0]-X[1])
             b = Y[0]-m*X[0]
-            horiz_intercepts.append(b)
-        else:
+            horiz_intercepts.append(b+m*big_lower_x)
+            horiz_lines.append(line)
+        elif math.fabs(t-math.pi/2.) <= 0.1:
             vert_list.append(line)
             m = (X[0]-X[1])/float(Y[0]-Y[1])
             b = X[0]-m*Y[0]
-            vert_intercepts.append(b)
+            vert_intercepts.append(b+m*big_lower_y)
+            vert_lines.append(line)
 
 
         # ax1.plot(X, Y,color="red")
@@ -158,20 +168,86 @@ for line in lines:
     #     print min(X)
     # print hesse_line(line)
 
+clusters = [-1 for i in range(len(horiz_lines))]
+clusters_count = -1
+
+horiz_lines.sort(key = lambda x:x[0][0])
+for l_index in range(len(horiz_lines)-1):
+    for l2_index in range(l_index+1,len(horiz_lines)):
+        line_1 = horiz_lines[l_index]
+        line_2 = horiz_lines[l2_index]
+
+        (x1,y1),(x2,y2) = line_1
+        (x3,y3),(x4,y4) = line_2
+
+        assert x1 <= x3
+
+        if x3 <= x2:
+            dist = y4 - y2
+            x_dist = 0
+        else:
+            m = (y2-y1)/float(x2-x1)
+            b = y2 - m*x2
+
+            d1 = (m*x3+b)-y3
+            d2 = (m*x4+b)-y4
+            dist = max(d1,d2)
+
+            x_dist = math.sqrt((x3-x2)**2+(y3-y2)**2)
+
+        # print dist
+        if (math.fabs(dist) < 0.3) and (x_dist < 100):
+            if (clusters[l_index] == -1) and (clusters[l2_index] == -1):
+                clusters_count += 1
+                clusters[l_index] = clusters_count
+                clusters[l2_index] = clusters_count
+            elif clusters[l_index] == -1:
+                clusters[l_index] = clusters[l2_index]
+            elif clusters[l2_index] == -1:
+                clusters[l2_index] = clusters[l_index]
+            else:
+                new_cluster_id = max(clusters[l2_index], clusters[l_index])
+                old_cluster_id = min(clusters[l2_index], clusters[l_index])
+                for j,c in enumerate(clusters):
+                    if c == old_cluster_id:
+                        clusters[j] = new_cluster_id
+
+
+for c in range(clusters_count):
+    pts = []
+    dist = 0
+    for i,l in enumerate(horiz_lines):
+        if clusters[i] == c:
+            pts.extend(l)
+            (x1,y1),(x2,y2) = l
+            dist += x2-x1
+    # print dist/float(big_upper_x-big_lower_x)
+    # if dist/float(big_upper_x-big_lower_x) < 0.6:
+    #     continue
+    if pts == []:
+        continue
+    pts.sort(key = lambda x:x[0])
+    X,Y = zip(*pts)
+    plt.plot(X,Y)
+
+plt.savefig("/home/ggdhines/Databases/example.jpg",bbox_inches='tight', pad_inches=0,dpi=72)
+assert False
+
 from sklearn.cluster import DBSCAN
 
-def fil_gaps(line_segments,horiz=True):
-    first_pt = line_segments[0][0]
-    last_pt = line_segments[-1][1]
+# def fil_gaps(line_segments,horiz=True):
+#     first_pt = line_segments[0][0]
+#     last_pt = line_segments[-1][1]
+#
+#     for l_index in range(len(line_segments)-1):
+#         l1 = line_segments[l_index]
+#         l2 = line_segments[l_index+1]
+#
+#         line_segments.append((l1[1],l2[0]))
+#
+#     return line_segments
 
-    for l_index in range(len(line_segments)-1):
-        l1 = line_segments[l_index]
-        l2 = line_segments[l_index+1]
-
-        line_segments.append((l1[1],l2[0]))
-
-    return line_segments
-
+# functions for sorting lines - has to deal with the fact that either lb_lines or ub_lines could be empty
 def lowest_y((lb_lines,ub_lines)):
     if lb_lines == []:
         assert ub_lines != []
@@ -189,27 +265,28 @@ def lowest_x((lb_lines,ub_lines)):
 
 # from
 # http://geospatialpython.com/2011/01/point-in-polygon.html
-def point_in_poly(x,y,poly):
-
-    n = len(poly)
-    inside = False
-
-    p1x,p1y = poly[0]
-    for i in range(n+1):
-        p2x,p2y = poly[i % n]
-        if y > min(p1y,p2y):
-            if y <= max(p1y,p2y):
-                if x <= max(p1x,p2x):
-                    if p1y != p2y:
-                        xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
-                    if p1x == p2x or x <= xints:
-                        inside = not inside
-        p1x,p1y = p2x,p2y
-
-    return inside
+# def point_in_poly(x,y,poly):
+#
+#     n = len(poly)
+#     inside = False
+#
+#     p1x,p1y = poly[0]
+#     for i in range(n+1):
+#         p2x,p2y = poly[i % n]
+#         if y > min(p1y,p2y):
+#             if y <= max(p1y,p2y):
+#                 if x <= max(p1x,p2x):
+#                     if p1y != p2y:
+#                         xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+#                     if p1x == p2x or x <= xints:
+#                         inside = not inside
+#         p1x,p1y = p2x,p2y
+#
+#     return inside
 
 
 def __pixel_generator__(boundary):
+    most_common_colour = [222,222,220]
     X,Y = zip(*boundary)
     x_min = int(math.ceil(min(X)))
     x_max = int(math.floor(max(X)))
@@ -222,25 +299,32 @@ def __pixel_generator__(boundary):
 
     for x in range(x_min,x_max+1):
         for y in range(y_min,y_max+1):
-            pass
-            print x,y,bbPath.contains_point((x,y))
+
+            if bbPath.contains_point((x,y)):
+                dist = math.sqrt(sum([(int(a)-int(b))**2 for (a,b) in zip(image[y][x],most_common_colour)]))
+                if dist > 40:
+                    plt.plot(x,y,"o",color="blue")
+
+    plt.xlim((x_min,x_max))
+    plt.ylim((y_max,y_min))
+
 
 def analysis(lines,intercepts,horiz=True):
     retval = []
     # d_list,t_list = zip(*hesse_list)
-    min_dist = min(intercepts)
-    max_dist = max(intercepts)
+    # min_dist = min(intercepts)
+    # max_dist = max(intercepts)
     # min_theta = min(t_list)
     # max_theta = max(t_list)
 
-    normalized_d = [[(d-min_dist)/float(max_dist-min_dist),] for d in intercepts]
+    # normalized_d = [[(d-min_dist)/float(max_dist-min_dist),] for d in intercepts]
     # normalized_t = [(t-min_theta)/float(max_theta-min_theta) for t in t_list]
     # print normalized_d
     # print normalized_t
 
     X = np.asarray([[i,] for i in intercepts])
     # print X
-    db = DBSCAN(eps=15, min_samples=1).fit(X)
+    db = DBSCAN(eps=1, min_samples=1).fit(X)
     core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
     core_samples_mask[db.core_sample_indices_] = True
     labels = db.labels_
@@ -256,8 +340,12 @@ def analysis(lines,intercepts,horiz=True):
             continue
 
         class_indices = [i for (i,l) in enumerate(labels) if l == k]
+        class_size = sum([1 for (i,l) in enumerate(labels) if l == k])
+        if class_size <= 2:
+            continue
         # print class_indices
         print len(class_indices)
+        print [intercepts[i] for (i,l) in enumerate(labels) if l == k]
 
         multiline = []
 
@@ -288,6 +376,9 @@ def analysis(lines,intercepts,horiz=True):
         lb_lines = []
         ub_lines = []
 
+        lower_distance = 0
+        upper_distance = 0
+
         # note that these line segments will often not be overlapping but we want the ones that are since they will
         # they may provide upper and lower bounds on a line.
         for l_index,line in enumerate(multiline):
@@ -299,6 +390,9 @@ def analysis(lines,intercepts,horiz=True):
                 (x1,y1),(x2,y2) = line
                 (x3,y3),(x4,y4) = line_2
 
+                # d1 = (x2-x1)
+                # d2 = (x4-x3)
+
                 # if line starts before line_2, make sure that it ends after line_2 has started
                 # or line starts before line_2 ends
                 # these two cases cover overlaps
@@ -308,30 +402,70 @@ def analysis(lines,intercepts,horiz=True):
                     if (y1 < y3) and (y2 < y4):
                         lb.add(l_index)
                         ub.add(l2_index)
+
+                        # lower_distance += d1
+                        # upper_distance += d2
                     elif (y1 > y3) and (y2 > y4):
                         lb.add(l2_index)
                         ub.add(l_index)
 
+                        # lower_distance += d2
+                        # upper_distance += d1
+
         lb = sorted(list(lb))
         ub = sorted(list(ub))
         for i in lb:
+            (x1,y1),(x2,y2) = multiline[i]
             if horiz:
                 # lb_lines.append(multiline[i])
                 lb_lines.extend(multiline[i])
+                d = x2 - x1
             else:
                 # else flip
-                (x1,y1),(x2,y2) = multiline[i]
+
                 lb_lines.extend(((y1,x1),(y2,x2)))
+                d = x2-x1
+            lower_distance += d
 
         for i in ub:
+            (x1,y1),(x2,y2) = multiline[i]
             if horiz:
                 ub_lines.extend(multiline[i])
+                d = x2 - x1
             else:
                 # else flip
-                (x1,y1),(x2,y2) = multiline[i]
+
                 ub_lines.extend([(y1,x1),(y2,x2)])
+                d = x2 - x1
+            upper_distance += d
 
 
+
+        if horiz:
+            D = big_upper_x-big_lower_x
+        else:
+            D = big_upper_y-big_lower_y
+
+        if lb_lines != []:
+            X,Y = zip(*lb_lines)
+            x_min = min(X)
+            x_max = max(X)
+            p1 = lower_distance/float(D)
+        else:
+            p1 = -1
+
+
+
+        if ub_lines != []:
+            X,Y = zip(*ub_lines)
+            x_min = min(X)
+            x_max = max(X)
+            p2 = upper_distance/float(D)
+        else:
+            p2 = -1
+
+        if max(p1,p2) < 0.5:
+            continue
 
         if horiz:
             if lb_lines != []:
@@ -383,10 +517,10 @@ def analysis(lines,intercepts,horiz=True):
             retval.append((lb_lines,ub_lines))
 
     if horiz:
-        assert isinstance(retval[0][0][0][1],int)
+        # assert isinstance(retval[0][0][0][1],int)
         retval.sort(key = lambda l:lowest_y(l))
     else:
-        assert isinstance(retval[0][0][0][0],int)
+        # assert isinstance(retval[0][0][0][0],int)
         retval.sort(key = lambda l:lowest_x(l))
     return retval
 
@@ -400,7 +534,8 @@ ax1.set_adjustable('box-forced')
 
 
 
-
+plt.savefig("/home/ggdhines/Databases/example.jpg",bbox_inches='tight', pad_inches=0,dpi=72)
+plt.close()
 
 
 for row_index in range(len(h_lines)-1):
@@ -408,6 +543,8 @@ for row_index in range(len(h_lines)-1):
         continue
 
     for column_index in range(len(v_lines)-1):
+        if column_index == 0:
+            continue
         if [] in v_lines[column_index]:
             continue
         # start with the top left
@@ -427,7 +564,7 @@ for row_index in range(len(h_lines)-1):
         x4_index,y4_index,(x4,y4) = multi_intersection(upper_horiz,lower_vert)
 
         # let's draw out the bounding box/polygon
-        offset = 0.005
+        offset = 0.01
         cell_boundries = [(x1+offset,y1+offset)]
         cell_boundries.extend([(x,y+offset) for (x,y) in lower_horiz[x1_index+1:x2_index+1]])
         cell_boundries.append((x2-offset,y2+offset))
@@ -438,13 +575,15 @@ for row_index in range(len(h_lines)-1):
         cell_boundries.extend([(x+offset,y) for (x,y) in reversed(lower_vert[y1_index+1:y4_index+1])])
         cell_boundries.append((x1+offset,y1+offset))
 
+
+        plt.close()
         X,Y = zip(*cell_boundries)
+        plt.plot(X,Y,color="red")
         __pixel_generator__(cell_boundries)
+        plt.show()
         assert False
 
         # now convert from points back to the list
 
 
 
-plt.savefig("/home/ggdhines/Databases/example.jpg",bbox_inches='tight', pad_inches=0,dpi=72)
-plt.close()
