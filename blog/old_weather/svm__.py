5e9bd71e1c7e4f1b372072b5393c4189e054e6a7
Greg <ggdhines@gmail.com>
2015-12-06 23:07:45 +0000

    exploring using svms to classify digits

---
+++ blog/old_weather/svm__.py
@@ -0,0 +1,33 @@
+
+
+# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
+# License: BSD 3 clause
+
+# Standard scientific Python imports
+import matplotlib.pyplot as plt
+
+# Import datasets, classifiers and performance metrics
+from sklearn import datasets, svm, metrics
+
+# The digits dataset
+digits = datasets.load_digits()
+
+
+# To apply a classifier on this data, we need to flatten the image, to
+# turn the data in a (samples, feature) matrix:
+n_samples = len(digits.images)
+data = digits.images.reshape((n_samples, -1))
+
+print data.shape
+
+# Create a classifier: a support vector classifier
+classifier = svm.SVC(gamma=0.001,probability=True)
+
+# We learn the digits on the first half of the digits
+classifier.fit(data[:n_samples / 2], digits.target[:n_samples / 2])
+
+# Now predict the value of the digit on the second half:
+expected = digits.target[n_samples / 2:]
+predicted = classifier.predict_proba(data[n_samples / 2:])
+
+print predicted
