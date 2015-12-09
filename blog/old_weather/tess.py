47447dc38f2dab5f32f2df9bbcab85ead57a373c
Greg Hines <greg@zooniverse.org>
2015-12-09 17:38:16 +0000

    see previous

--- blog/old_weather/tess.py
+++ blog/old_weather/tess.py
@@ -7,15 +7,15 @@ import xmltodict
 import matplotlib.cbook as cbook
 import cv2
 
-img = cv2.imread("/home/ggdhines/Databases/old_weather/test_cases/Bear-AG-29-1941-0813.JPG")
+img = cv2.imread("/home/ggdhines/Databases/old_weather/aligned_images/Bear-AG-29-1939-0245.JPG")
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(gray,175,255,cv2.THRESH_BINARY_INV)
-cv2.imwrite("/home/ggdhines/greg.jpg",thresh)
+# cv2.imwrite("/home/ggdhines/greg.jpg",thresh)
 # assert False
 # call(["tesseract","/home/ggdhines/Downloads/Hooper_John-Certeine_comfortable_expositions-STC-13743-548_05-p3.tif","/home/ggdhines/tess","hocr"])
 
 # image_file = cbook.get_sample_data("/home/ggdhines/Databases/old_weather/test_cases/Bear-AG-29-1941-0813.JPG")
-image_file = cbook.get_sample_data("/home/ggdhines/greg.jpg")
+image_file = cbook.get_sample_data("/home/ggdhines/Databases/old_weather/aligned_images/Bear-AG-29-1939-0245.JPG")
 image = plt.imread(image_file)
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
@@ -78,7 +78,7 @@ def process_word(node):
     else:
         text = node.text
 
-    if True:#text != " ":
+    if text != " ":
         title = node.attrib["title"]
         title_params = title.split()
         coords = title_params[1:4]
@@ -157,19 +157,19 @@ for child in root[1]:
         assert False
 
 
-with open("/home/ggdhines/Documents/gold_standard.txt","rb") as f:
-    gold_paragraphs = []
-    current_p = ""
-    for l in f.readlines():
-        l = l.strip()
-        if l != "":
-            current_p += l + " "
-        else:
-            gold_paragraphs.append(current_p[:-1])
-            current_p = ""
-
-    if current_p != "":
-        gold_paragraphs.append(current_p[:-1])
+# with open("/home/ggdhines/Documents/gold_standard.txt","rb") as f:
+#     gold_paragraphs = []
+#     current_p = ""
+#     for l in f.readlines():
+#         l = l.strip()
+#         if l != "":
+#             current_p += l + " "
+#         else:
+#             gold_paragraphs.append(current_p[:-1])
+#             current_p = ""
+#
+#     if current_p != "":
+#         gold_paragraphs.append(current_p[:-1])
 
     # print gold_paragraphs
     # print ocr
