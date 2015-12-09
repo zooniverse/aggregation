cbbf6667a3f8f23aea3eee9fc2f858eed4a78585
Greg Hines <greg@zooniverse.org>
2015-12-09 17:37:41 +0000

    just having fun with blank images in snapshot serengeti

--- experimental/serengeti/blanks.py
+++ experimental/serengeti/blanks.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 __author__ = 'greghines'
+import matplotlib
+matplotlib.use('WXAgg')
 import numpy as np
 import os
 import pymongo
@@ -12,22 +14,68 @@ image_directory = "/home/greg/Databases/chimp/images/"
 
 # connect to the mongodb server
 client = pymongo.MongoClient()
-db = client['serengeti_2015-02-22']
+db = client['b']
 subjects = db["serengeti_subjects"]
-classifications = db["serengeti_classifications"]
+
+all_files = []
+
+blanks = {}
+images_to_subjects = {}
+
+for ii,s in enumerate(subjects.find({"tutorial":{"$ne":True},"coords":[-2.4672743413359295, 34.75278520232197]}).limit(20)):
+    print ii
+    id_ = s["zooniverse_id"]
+    blanks[id_] = []
+
+    for url in s["location"]["standard"]:
+        r_slash = url.rfind("/")
+        fname = url[r_slash+1:]
+
+        image_path = "/home/ggdhines/Databases/images/"+fname
+
+        all_files.append(fname)
+
+        images_to_subjects[fname] = id_
+
+        if not(os.path.isfile(image_path)):
+            urllib.urlretrieve(url, image_path)
+
+print len(all_files)
+for file_index in range(len(all_files)):
+    good_count = 0
+    img1 = cv2.imread("/home/ggdhines/Databases/images/"+all_files[file_index],0)          # queryImage
+    for file2_index in range(len(all_files)):
+        if file_index == file2_index:
+            continue
 
 
-for ii,s in enumerate(classifications.find({"tutorial":{"$ne":True}}).limit(100)):
-    if not("started" in s["annotations"][-1]):
-        continue
+        img2 = cv2.imread("/home/ggdhines/Databases/images/"+all_files[file2_index],0) # trainImage
 
-    print s["metadata"]
-    print s["user_ip"],s["annotations"]
-    print subjects.find_one()
-# for ii,s in enumerate(subjects.find({"tutorial":{"$ne":True}}).limit(10)):
-#     # print s["coords"],s["created_at"]
-#     print s["coords"]
-#     print s["metadata"]["timestamps"][0]
-#     images = s["location"]["standard"]
-#     slash_indices =
-#     print
+        # Initiate SIFT detector
+        sift = cv2.xfeatures2d.SURF_create()
+
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = sift.detectAndCompute(img1,None)
+        kp2, des2 = sift.detectAndCompute(img2,None)
+
+        # BFMatcher with default params
+        bf = cv2.BFMatcher()
+        matches = bf.knnMatch(des1,des2, k=2)
+
+        # Apply ratio test
+        good = []
+        for m,n in matches:
+            print type(m)
+            assert False
+            if m.distance < 0.2*n.distance:
+                good.append([m])
+
+        # img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
+        # print len(good)
+        # plt.imshow(img3),plt.show()
+
+        if len(good) > 500:
+            good_count += 1
+
+    print "/home/ggdhines/Databases/images/"+all_files[file_index],good_count
+
