11bcfea7a9bd2d3dcebee81a8131361523d34a80
Greg Hines <greg@zooniverse.org>
2016-01-05 16:09:59 +0000

    removes all references to "reduction"

--- setup.py
+++ setup.py
@@ -11,17 +11,15 @@ class build_ext(_build_ext):
         self.include_dirs.append(numpy.get_include())
 
 setup(
-        name='reduction',
-        version='0.1.0',
-        author='Edward Paget',
-        author_email='ed@zooniverse.org',
-        packages=['reduction', 'reduction.adapters', 'reduction.algos'] ,
-        scripts=['bin/zoo-reduce'],
+        name='aggregation',
+        version='0.2.0',
+        author='Greg Hines',
+        author_email='greg@zooniverse.org',
         url='http://github.com/zooniverse/reduction',
         license='LICENSE',
         cmdclass={'build_ext':build_ext},
         setup_requires=['numpy==1.8.1'],
-        description='Reduces crowdsourced data from Zooniverse projects',
+        description='Aggregation for Zooniverse projects',
         long_description=open('README.md').read(),
         install_requires=[
             "networkx==1.8.1",
