b2370d9da5134b79e8671f4efc70391138f4c4e7
Greg Hines <greg@zooniverse.org>
2016-01-07 21:41:57 +0000

    honestly not sure

--- experimental/serengeti/blanks.py
+++ experimental/serengeti/blanks.py
@@ -8,22 +8,13 @@ import pymongo
 import urllib
 import matplotlib.pyplot as plt
 import cv2
-import math
+
 # the directory to store the movie preview clips in
 image_directory = "/home/greg/Databases/chimp/images/"
 
-def get_gradient(im) :
-    # Calculate the x and y gradients using Sobel operator
-    grad_x = cv2.Sobel(im,cv2.CV_32F,1,0,ksize=3)
-    grad_y = cv2.Sobel(im,cv2.CV_32F,0,1,ksize=3)
-
-    # Combine the two gradients
-    grad = cv2.addWeighted(np.absolute(grad_x), 0.5, np.absolute(grad_y), 0.5, 0)
-    return grad
-
 # connect to the mongodb server
 client = pymongo.MongoClient()
-db = client['serengeti_2015-08-20']
+db = client['b']
 subjects = db["serengeti_subjects"]
 
 all_files = []
@@ -47,51 +38,44 @@ for ii,s in enumerate(subjects.find({"tutorial":{"$ne":True},"coords":[-2.467274
         images_to_subjects[fname] = id_
 
         if not(os.path.isfile(image_path)):
-            prefix = "http://zooniverse-static.s3.amazonaws.com/"
-            urllib.urlretrieve(prefix+url[7:], image_path)
+            urllib.urlretrieve(url, image_path)
 
-# print len(all_files)
-# for file_index in range(len(all_files)):
-#     good_count = 0
-#     img1 = cv2.imread("/home/ggdhines/Databases/images/"+all_files[file_index],0)          # queryImage
-#     for file2_index in range(len(all_files)):
-#         print file2_index
-#         if file_index == file2_index:
-#             continue
-#
-#
-#         img2 = cv2.imread("/home/ggdhines/Databases/images/"+all_files[file2_index],0) # trainImage
-#
-#         warp_mode = cv2.MOTION_AFFINE
-#         warp_matrix = np.eye(2, 3, dtype=np.float32)
-#         criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 5000,  1e-10)
-#         (cc, warp_matrix) = cv2.findTransformECC (get_gradient(img1), get_gradient(img2),warp_matrix, warp_mode, criteria)
-#
-#     print "/home/ggdhines/Databases/images/"+all_files[file_index],good_count
+print len(all_files)
+for file_index in range(len(all_files)):
+    good_count = 0
+    img1 = cv2.imread("/home/ggdhines/Databases/images/"+all_files[file_index],0)          # queryImage
+    for file2_index in range(len(all_files)):
+        if file_index == file2_index:
+            continue
 
-img = cv2.imread('simple.jpg',0)
 
-# Initiate FAST object with default values
-fast = cv2.FastFeatureDetector()
+        img2 = cv2.imread("/home/ggdhines/Databases/images/"+all_files[file2_index],0) # trainImage
 
-# find and draw the keypoints
-kp = fast.detect(img,None)
-img2 = cv2.drawKeypoints(img, kp, color=(255,0,0))
+        # Initiate SIFT detector
+        sift = cv2.xfeatures2d.SURF_create()
 
-# Print all default params
-print "Threshold: ", fast.getInt('threshold')
-print "nonmaxSuppression: ", fast.getBool('nonmaxSuppression')
-print "neighborhood: ", fast.getInt('type')
-print "Total Keypoints with nonmaxSuppression: ", len(kp)
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = sift.detectAndCompute(img1,None)
+        kp2, des2 = sift.detectAndCompute(img2,None)
 
-cv2.imwrite('fast_true.png',img2)
+        # BFMatcher with default params
+        bf = cv2.BFMatcher()
+        matches = bf.knnMatch(des1,des2, k=2)
 
-# Disable nonmaxSuppression
-fast.setBool('nonmaxSuppression',0)
-kp = fast.detect(img,None)
+        print type(matches)
+        # Apply ratio test
+        good = []
+        for m,n in matches:
+            print m.distance,n.distance
+            if m.distance < 0.2:
+                good.append([m])
 
-print "Total Keypoints without nonmaxSuppression: ", len(kp)
+        img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
+        print len(good)
+        plt.imshow(img3),plt.show()
 
-img3 = cv2.drawKeypoints(img, kp, color=(255,0,0))
+        if len(good) > 500:
+            good_count += 1
 
-cv2.imwrite('fast_false.png',img3)
+    print "/home/ggdhines/Databases/images/"+all_files[file_index],good_count
+
