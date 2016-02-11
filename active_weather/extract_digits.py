c5536c1f13f1daa620fa254513e35843114c4f3a
Greg Hines <greg@zooniverse.org>
2016-02-11 17:02:47 +0000

    moved over to main active weather repo and add in better thresholding

--- active_weather/extract_digits.py
+++ active_weather/extract_digits.py
@@ -17,7 +17,7 @@ training = mndata.load_training()
 weight = "distance"
 clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weight)
 
-pca = PCA(n_components=50)
+pca = PCA(n_components=80)
 T = pca.fit(training[0])
 reduced_training = T.transform(training[0])
 print sum(pca.explained_variance_ratio_)
@@ -30,46 +30,66 @@ def line_removal(pts,num_col,num_row):
 
 
 def extract(image):
+    digits = []
+    confidence = []
+    x_location = []
     assert isinstance(image,np.ndarray)
-    num_col,num_row,_ = image.shape
-    print num_col,num_row
+    num_col,num_row = image.shape
 
-    colours = {}
+    # image = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+    # blur = cv2.GaussianBlur(image,(5,5),0)
+    _,image = cv2.threshold(image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-    for c in range(num_col):
-        for r in range(num_row):
-            pixel_colour = tuple(image[c,r])
-            if pixel_colour not in colours:
-                colours[pixel_colour] = 1
-            else:
-                colours[pixel_colour] += 1
+    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    cv2.imshow('image',image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-    most_common_colour,_ = sorted(colours.items(),key = lambda x:x[1],reverse=True)[0]
-    pts = []
-    for c in range(num_col):
-        for r in range(num_row):
-            pixel_colour = tuple(image[c,r])
+    pts = np.where(image>0)
 
-            dist = math.sqrt(sum([(int(a)-int(b))**2 for (a,b) in zip(pixel_colour,most_common_colour)]))
+    # # assert False
+    #
+    # colours = {}
+    #
+    # for c in range(num_col):
+    #     for r in range(num_row):
+    #         pixel_colour = tuple(image[c,r])
+    #         if pixel_colour not in colours:
+    #             colours[pixel_colour] = 1
+    #         else:
+    #             colours[pixel_colour] += 1
+    #
+    # most_common_colour,_ = sorted(colours.items(),key = lambda x:x[1],reverse=True)[0]
+    # pts = []
+    # for c in range(num_col):
+    #     for r in range(num_row):
+    #         pixel_colour = tuple(image[c,r])
+    #
+    #         dist = math.sqrt(sum([(int(a)-int(b))**2 for (a,b) in zip(pixel_colour,most_common_colour)]))
+    #
+    #         if dist > 40:
+    #             # plt.plot(r,c,"o",color="black")
+    #             pts.append((r,c))
 
-            if dist > 40:
-                # plt.plot(r,c,"o",color="black")
-                pts.append((r,c))
+    # x,y = zip(*pts)
+    # plt.plot(x,y,"o")
+    # plt.show()
 
     # hopefully corresponds to an empty cell
     if pts == []:
-        return
+        return [],[]
 
-    rows,columns = zip(*pts)
+    rows,columns = pts
     min_r =min(rows)
     max_r =max(rows)
 
     min_c =min(columns)
     max_c =max(columns)
 
-    pts = np.asarray([(r,c) for (r,c) in pts if (r>(min_r+2))and(r<(max_r-2))and(c>(min_c+2))and(c<(max_c-2))])
+    # pts = np.asarray([(r,c) for (r,c) in pts if (r>(min_r))and(r<(max_r-2))and(c>(min_c+2))and(c<(max_c-2))])
+    pts = np.asarray([(r,c) for (r,c) in zip(rows,columns) if (r>=min_r)and(r<=max_r)and(c>=min_c)and(c<=max_c)])
 
-    db = DBSCAN(eps=3, min_samples=20).fit(pts)
+    db = DBSCAN(eps=3, min_samples=5).fit(pts)
     labels = db.labels_
     unique_labels = set(labels)
 
@@ -84,6 +104,9 @@ def extract(image):
 
         xy = pts[class_member_mask]
         X_l,Y_l = zip(*xy)
+
+        x_location.append(np.median(X_l))
+
         max_x = max(X_l)
         max_y = max(Y_l)
 
@@ -133,18 +156,16 @@ def extract(image):
 
         centered_array = [0 for i in range(28**2)]
 
-        print digit_array == digit_image
-        print "===----"
         try:
             darkest_pixel = 0
             for y in range(len(digit_array)):
                 for x in range(len(digit_array[0])):
                     darkest_pixel = max(darkest_pixel,digit_array[y][x])
         except TypeError:
-            print "problem skipping this one"
+            # print "problem skipping this one"
             continue
 
-        darkest_pixel = max(darkest_pixel,100)
+        # darkest_pixel = max(darkest_pixel,100)
 
         for y in range(len(digit_array)):
             for x in range(len(digit_array[0])):
@@ -158,25 +179,38 @@ def extract(image):
                 else:
                     centered_array[(y+y_offset)*28+(x+x_offset)] = 0
 
-        for index,i in enumerate(centered_array):
-            if i > 0:
-                x = index%28
-                y = index/28
-                plt.plot(x,y,"o",color="blue")
-
-        plt.ylim((28,0))
-        plt.xlim((0,28))
-        plt.savefig("/home/ggdhines/tmp.png")
+        # for index,i in enumerate(centered_array):
+        #     if i > 0:
+        #         x = index%28
+        #         y = index/28
+        #         plt.plot(x,y,"o",color="blue")
+        #
+        # plt.ylim((28,0))
+        # plt.xlim((0,28))
+        # plt.savefig("/home/ggdhines/tmp.png")
+        # plt.close()
 
         centered_array = np.asarray(centered_array)
         # print centered_array.shape
         centered_array = T.transform(centered_array)
-        print centered_array
-        print clf.predict_proba(centered_array)
-        raw_input("enter something")
+        # print centered_array
+        probabilities = clf.predict_proba(centered_array)
+        confidence.append(np.max(probabilities))
+        digits.append(str(int(np.argmax(probabilities))))
+
+
+
+        # raw_input("enter something")
 
     # plt.xlim((0,num_row))
     # plt.ylim((num_col,0))
     # line_removal(pts,num_col,num_row)
     # plt.savefig("/home/ggdhines/tmp.png")
     # plt.close()
+    if digits == []:
+        return [],[]
+    digits_confidence_location = zip(digits,confidence,x_location)
+    digits_confidence_location.sort(key = lambda x:x[2])
+    digits,confidence,_ = zip(*digits_confidence_location)
+
+    return digits,confidence
