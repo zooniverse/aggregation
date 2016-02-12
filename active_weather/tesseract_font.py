19640df861832f324524a97adba8ee8a02041750
Greg <ggdhines@gmail.com>
2016-02-12 11:57:53 +0000

    box file creation working :)

--- active_weather/tesseract_font.py
+++ active_weather/tesseract_font.py
@@ -26,23 +26,85 @@ n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 
 clusters = []
 
+height_list = []
+width_list = []
+
 print n_clusters_
 unique_labels = set(labels)
 colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+
+# start by just ordering the clusters so we can match them up to their characters
 for k, col in zip(unique_labels, colors):
 
     class_member_mask = (labels == k)
     xy = X[class_member_mask]
 
-    min_x,min_y = np.min(xy,axis=0)
-    max_x,max_y = np.max(xy,axis=0)
-
-    height = max_y-min_y+1
-    width = max_x-min_x +1
-
     clusters.append(xy)
 
 clusters.sort(key = lambda c:np.mean(c[:,0]))
 
-plt.plot(clusters[0][:,0],clusters[0][:,1],".")
-plt.show()
+for cluster_index,xy in enumerate(clusters):
+    min_x,min_y = np.min(xy,axis=0)
+    max_x,max_y = np.max(xy,axis=0)
+
+
+
+    height = max_y-min_y+1
+    width = max_x-min_x +1
+
+    print min_x,min_y,height,width
+
+    height_list.append(height)
+    width_list.append(width)
+
+    xy[:,0] -= min_x
+    xy[:,1] -= min_y
+
+    clusters[cluster_index] = xy
+
+spacing = 35
+
+overall_width = 10*max(width_list) + spacing*11
+overall_height = sum(height_list) + spacing*len(characters)
+
+print overall_height,overall_width
+
+template_image = np.zeros((overall_height,overall_width),np.uint8)
+template_image.fill(255)
+
+# cv2.imwrite("/home/ggdhines/fonts.jpg",template_image)
+
+print template_image
+
+print template_image.shape
+height_offset = 0
+
+with open("/home/ggdhines/active.basic.exp0.box","w") as f:
+    for i,c in enumerate(characters):
+        # char_mask = np.zeros((height_list[i],width_list[i]),np.uint8)
+        char_mask = np.zeros((height_list[i],width_list[i]),np.uint8)
+        char_mask.fill(255)
+
+        # todo - do this better
+        for x,y in clusters[i]:
+            char_mask[y,x] = 0
+
+        height = height_list[i]
+        width = width_list[i]
+
+        print char_mask.shape
+
+        width_offset = spacing
+        for column in range(10):
+            template_image[height_offset:height_offset+height,width_offset:width_offset+width] = char_mask
+
+            f.write(c)
+            f.write(" "+str(width_offset)+" "+str(overall_height - height_offset-height))
+            f.write(" "+str(width_offset+width)+" "+str(overall_height - height_offset) + " 0\n")
+
+            width_offset += width + spacing
+
+        height_offset += height + spacing
+
+
+cv2.imwrite("/home/ggdhines/active.basic.exp0.tiff",template_image)
