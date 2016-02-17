6b6eaee761dbde8af75b465b8c486f6139a2858d
Greg Hines <greg@zooniverse.org>
2016-02-17 16:52:09 +0000

    cleaning up

--- engine/clustering.py
+++ engine/clustering.py
@@ -96,15 +96,7 @@ class Cluster:
                 if shape != self.shape:
                     continue
 
-                if self.project is not None:
-                    if self.project.environment == "development":
-                        limit = -1
-                    else:
-                        limit = 500
-                else:
-                    limit = -1
-
-                for subject_count,subject_id in list(enumerate(raw_markings[task_id][shape]))[:limit]:
+                for subject_count,subject_id in list(enumerate(raw_markings[task_id][shape])):
 
                     print subject_count,subject_id
                     assert raw_markings[task_id][shape][subject_id] != []
