21204eedd695be578a097435d0454355f93a9000
Greg <ggdhines@gmail.com>
2016-03-24 16:56:20 +0000

    renamed - should now be obvious that this is the main preprocessing file

--- docs/source/examples/penguin.py
+++ docs/source/examples/penguin.py
@@ -1,4 +1,8 @@
 import pymongo
+import sys
+sys.path.append("/home/ggdhines/github/aggregation/engine")
+sys.path.append("/home/ggdhines/Pycharm/reduction/engine")
+from agglomerative import Agglomerative
 
 client = pymongo.MongoClient()
 db = client['penguin']
@@ -12,16 +16,16 @@ subject_collection = db["penguin_subjects"]
 #     classification_collection.update_one({"_id":_id},{"$set":{"zooniverse_id":zooniverse_id}})
 
 
-# result = db.profiles.create_index([('zooniverse_id', pymongo.ASCENDING)],unique=False)
+result = db.profiles.create_index([('zooniverse_id', pymongo.ASCENDING)],unique=False)
+print result
+for c in classification_collection.find().limit(10):
+    _id = c["_id"]
+    zooniverse_id = c["subjects"][0]["zooniverse_id"]
+    print zooniverse_id
 
-# for c in classification_collection.find().limit(10):
-#     _id = c["_id"]
-#     zooniverse_id = c["subjects"][0]["zooniverse_id"]
-#
-#     for c2 in classification_collection.find({"zooniverse_id":zooniverse_id}):
-#         print c2
+    for c2 in classification_collection.find({"zooniverse_id":zooniverse_id}):
+        print c2
 
 
-import sys
-sys.path.append("/home/ggdhines/github/aggregation/engine")
-from agglomerative import Agglomerative
+
+
