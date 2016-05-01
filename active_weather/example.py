6c8a85bad8345c999c794ec924750bc49be8191b
Greg Hines <greg@zooniverse.org>
2016-05-01 12:42:42 +0100

    using svms to classify contours

--- active_weather/example.py
+++ active_weather/example.py
@@ -7,6 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from active_weather import __otsu_bin__,__pca__,__create_mask__
 import cPickle, gzip
+import math
 
 img = cv2.imread("/home/ggdhines/eastwind-wag-279-1946_0523-0.JPG")
 
@@ -40,32 +41,63 @@ train_set, valid_set, test_set = cPickle.load(f)
 f.close()
 print(train_set)
 n_samples = len(train_set[0])
-print(n_samples)
+# print(n_samples)
+# print(train_set[0][0])
+# assert False
 data = train_set[0].reshape((n_samples, -1))
 
-classifier = svm.SVC(probability=True,gamma=0.001)
+classifier = svm.SVC(probability=True,gamma=1)
 
 # We learn the digits on the first half of the digits
-# classifier.fit(data[:5000], train_set[1][:5000])
+classifier.fit(data[:5000], train_set[1][:5000])
+
+big_list = []
 
 _,contour, hier = cv2.findContours(masked_image.copy(),cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
-for cnt in contour:
+for cnt,h in zip(contour,hier[0]):
     perimeter = cv2.arcLength(cnt,True)
     if perimeter < 100:
         continue
-    print(perimeter)
+    if h[-1] != -1:
+        continue
+
+    if len(big_list) == 3:
+        break
+
     cnt = cnt.reshape((cnt.shape[0],2))
 
     bottom,left = np.min(cnt,axis=0)
-
-    # cnt = cnt - (bottom,left)
-
     top,right = np.max(cnt,axis=0)
-    canvas = np.zeros(table.shape,np.uint8)
-    canvas.fill(255)
-    cv2.drawContours(canvas, [cnt], 0, 0, -1)
+
+    canvas = np.zeros(table.shape[:2],np.uint8)
+    # canvas.fill(255)
+    cv2.drawContours(canvas, [cnt], 0, 1, -1)
+
+    # plt.show()
+
+    character = canvas[left:right,bottom:top]
+    # plt.imshow(character,cmap="gray")
+    # plt.show()
+
+    rescaled = cv2.resize(character,(28,28)).astype(np.float)
+    # print(rescaled.shape)
+    # plt.imshow(rescaled,cmap="gray")
+    # plt.show()
+
+    flattened_character = rescaled.flatten()
+    print(flattened_character[:100])
+    big_list.append(flattened_character)
+    continue
+
+    predicted = classifier.predict([flattened_character])
+    probabilities = classifier.predict_proba([flattened_character])
+    print(predicted)
+    print(probabilities)
+
     plt.imshow(canvas,cmap="gray")
     plt.show()
 
+print(np.asarray(big_list).shape)
+print(classifier.predict_proba(np.asarray(big_list)))
 
 
