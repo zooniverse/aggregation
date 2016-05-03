af5939a9e4885cf309ad2c0c2e837209c53d2073
Greg <ggdhines@gmail.com>
2016-05-03 12:56:46 +0100

    trying to get tesseract working again

--- active_weather/learning.py
+++ active_weather/learning.py
@@ -8,28 +8,32 @@ import random
 from tesseract_update import TesseractUpdate
 import random
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import warnings
 __author__ = 'ggdhines'
 
+warnings.filterwarnings('error')
 
-def weighted_choice(choices):
-# http://stackoverflow.com/questions/3679694/a-weighted-version-of-random-choice
-    total = sum(w for c, w in choices)
-    r = random.uniform(0, total)
-    upto = 0
-    for c, w in choices:
-        if upto + w >= r:
-            return c
-        upto += w
-    assert False, "Shouldn't get here"
 
-def boltzman_sampling(confidences):
-    temp = 30
-    values = [math.exp(c/temp) for c in confidences]
-
-    # print(values)
-    summed = sum(values)
-    normalized_values = [c/float(summed) for c in values]
-    return list(enumerate(normalized_values))
+# def weighted_choice(choices):
+# # http://stackoverflow.com/questions/3679694/a-weighted-version-of-random-choice
+#     total = sum(w for c, w in choices)
+#     r = random.uniform(0, total)
+#     upto = 0
+#     for c, w in choices:
+#         if upto + w >= r:
+#             return c
+#         upto += w
+#     assert False, "Shouldn't get here"
+#
+# def boltzman_sampling(confidences):
+#     temp = 30
+#     values = [math.exp(c/temp) for c in confidences]
+#
+#     # print(values)
+#     summed = sum(values)
+#     normalized_values = [c/float(summed) for c in values]
+#     return list(enumerate(normalized_values))
 
 base = "/home/ggdhines/Databases/old_weather/aligned_images/Bear/1940/"
 region_bound = (559,3282,1276,2097)
@@ -43,158 +47,212 @@ masked_image = None
 
 tess = TesseractUpdate()
 tess.__create_blank_page__()
-#
+
+gold_standard_characters = {}
+
 # <<<<<<< 8ce8e3a4c7d9552551e469bcbd4315f395525d63
-# for char in "A":#BCDEFGHIJKLMNOPQRSTUVWXYZ-1234567890.abcdefghijkmnopqrstuvwxyz":
-# =======
-# current_subject_id = None
-#
-# big_dict = {}
-#
-# characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ-1234567890.abcdefghijkmnopqrstuvwxyz"
-# total_character_list = []
-# for c in characters:
-#     total_character_list.extend([c for _ in range(10)])
-# random.shuffle(total_character_list)
+for char in "A":#BCDEFGHIJKLMNOPQRSTUVWXYZ-1234567890.abcdefghijkmnopqrstuvwxyz":
+    try:
+        cur.execute("select * from characters where characters = \"" + char + "\" order by confidence desc")
+
+        all_heights = []
+        all_widths = []
+        for r in cur.fetchall()[:150]:
+            subject_id, region, column, row, characters, confidence, lb_x, ub_x, lb_y, ub_y = r
+
+            all_heights.append(ub_y-lb_y+1)
+            all_widths.append(ub_x-lb_x+1)
+
+        if all_widths == []:
+            continue
+
+        desired_width = int(np.median(all_widths))
+        desired_height = int(np.median(all_heights))
+
+        print(desired_width,desired_height)
+
+        bitmaps = []
+        cur.execute("select * from characters where characters = \"" + char + "\" order by confidence desc")
+        for r in cur.fetchall()[:30]:
+            subject_id, region, column, row, characters, confidence, lb_x, ub_x, lb_y, ub_y = r
+
+            masked_image = cv2.imread("/home/ggdhines/to_upload3/" + subject_id + ".jpg", 0)
+            char_image = masked_image[lb_y:ub_y + 1, lb_x:ub_x + 1]
+
+            resized_char = cv2.resize(char_image,(desired_width,desired_height))
+            bitmaps.append(resized_char.flatten())
+
+        bitmaps = np.asarray(bitmaps)
+        pca = PCA(n_components=1)
+
+        X_r = pca.fit(bitmaps).transform(bitmaps)
+        average_reduced_char = np.mean(X_r)
+
+        average_char = pca.inverse_transform(average_reduced_char)
+        average_char = average_char.reshape((desired_height,desired_width))
+        cv2.normalize(average_char, average_char, 0, 255, cv2.NORM_MINMAX)
+        average_char = average_char.astype(np.uint8)
+
+
+        ret2, th2 = cv2.threshold(average_char, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        gold_standard_characters[char] = th2
+
+    except RuntimeWarning as e:
+        print("skipping: " + str(char))
+        print(e)
+
+
+characters_with_gold_standard = gold_standard_characters.keys()
+
+
+total_character_list = []
+for c in characters_with_gold_standard:
+    total_character_list.extend([c for _ in range(10)])
+random.shuffle(total_character_list)
 # for ii,char in enumerate(total_character_list):
 #     if (ii>0) and (ii%30 == 0):
 #         print("output")
 #         tess.__next_image__()
 # >>>>>>> active weather updates
 
-for char in "F.":
-    # cur.execute("select * from characters where characters = \""+c+"\" and confidence < 80")
-    # for r in cur.fetchall():
-    #     subject_id,region,column,row,characters,confidence,lb_x,ub_x,lb_y,ub_y = r
-    #
-    #     if subject_id != current_subject:
-    #         current_subject = subject_id
-    #         fname = base + subject_id + ".JPG"
-    #         image = cv2.imread(fname)
-    #
-    #         sub_image = image[region_bound[2]:region_bound[3],region_bound[0]:region_bound[1]]
-    #
-    #         gray = cv2.cvtColor(sub_image,cv2.COLOR_BGR2GRAY)
-    #         # masked_image = preprocessing.__mask_lines__(gray)
-    #
-    #         height,width,_ = sub_image.shape
-    #
-    #     char_image = gray[lb_y:ub_y+1,lb_x:ub_x+1]
-    #
-    #     plt.imshow(char_image,cmap="gray")
-    #     plt.title(characters + " -- " + str(confidence))
-    #     plt.show()
-    # continue
+for char in total_character_list:
+    tess.__add_char__(char, gold_standard_characters[char])
 
-    # cur.execute("select count(*) from characters where characters = \""+char+"\"")
-    # a = cur.fetchone()
-    # cur.execute("select count(*) from characters where characters = \""+char+"\" and confidence > 80")
-    # b = cur.fetchone()
-    # if (a[0] > 0) and (b[0] == 0):
-    #     raw_input((char,a))
-    # continue
-
-
-    # cur.execute("select * from characters where characters = \""+char+"\" and confidence < 80 and confidence > 70")
-    cur.execute("select * from characters where characters = \""+char+"\" order by confidence desc")
-
-    examples = []
-    confidence_values = []
-    height_values = []
-    width_values = []
-
-    for r in cur.fetchall()[:150]:
-        subject_id,region,column,row,characters,confidence,lb_x,ub_x,lb_y,ub_y = r
-        # print(confidence)
-        # if subject_id != current_subject:
-        #     current_subject = subject_id
-        #     fname = base + subject_id + ".JPG"
-        #     image = cv2.imread(fname)
-        #
-        #     sub_image = image[region_bound[2]:region_bound[3],region_bound[0]:region_bound[1]]
-        #
-        #     gray = cv2.cvtColor(sub_image,cv2.COLOR_BGR2GRAY)
-        #     masked_image = preprocessing.__mask_lines__(gray)
-
-        if subject_id != current_subject_id:
-            masked_image = cv2.imread("/home/ggdhines/to_upload3/"+subject_id+".jpg",0)
-            current_subject_id = subject_id
-        height,width = masked_image.shape
-
-        char_image = masked_image[lb_y:ub_y+1,lb_x:ub_x+1]
-
-        # plt.imshow(char_image,cmap="gray")
-        # plt.show()
-        #
-        # input_ = raw_input("is it correct?(y/n) ")
-        # if input_ == "n":
-        #     continue
-        # assert input_ == "y"
-
-        chr_height = ub_y-lb_y+1
-        chr_width = ub_x-lb_x+1
-
-        examples.append(char_image)
-        confidence_values.append(confidence)
-        height_values.append(chr_height)
-        width_values.append(chr_width)
-
-    # raw_input("enter")
-
-    if confidence_values == []:
-        continue
-
-    # combined = zip(confidence_values,examples,height_values,width_values)
-    # combined.sort(key=lambda x:x[0],reverse=True)
-    # confidence_values,examples,height_values,width_values = zip(*combined[:150])
-
-    if height_values != []:
-        print("for character : " + char + " we have " + str(len(height_values)) + " samples")
-
-        desired_height = int(np.median(height_values))
-        desired_width = int(np.median(width_values))
-
-        weights = boltzman_sampling(confidence_values)
-
-        rescaled_examples = []
-        for c in examples:
-            c2 = cv2.resize(c,(desired_height,desired_width))
-            # plt.imshow(c2)
-            # plt.show()
-
-            rescaled_examples.append(c2)
-
-        for i in range(1):
-            num_samples = len(weights)
-            samples = [weighted_choice(weights) for i in range(max(1,min(int(num_samples/2.),20)))]
-
-
-            image_list = [rescaled_examples[j] for j in samples]
-            s = image_list[0].shape
-
-            # flatten_list = [im.reshape((im.shape[0]*im.shape[1])) for im in image_list]
-            # pca = PCA(n_components=1)
-            # pca_images = pca.fit_transform(flatten_list)
-            # print(sum(pca.explained_variance_ratio_))
-
-            average_image = np.median(image_list,axis=0)
-
-
-            # pca_average = np.median(pca_images,axis=0)
-            # new_average = pca.inverse_transform(pca_average)
-            # average_image = new_average.reshape(s)
-
-            average_image = cv2.normalize(average_image,average_image,0,255,cv2.NORM_MINMAX)
-            average_image = average_image.astype(np.uint8)
-            # print(average_image)
-
-            # ret,close = cv2.threshold(average_image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-
-            # kernel = np.ones((10,10),np.uint8)
-            # close = cv2.morphologyEx(close,cv2.MORPH_DILATE,kernel,iterations = 1)
-
-            tess.__add_char__(char,average_image)
-
-        # raw_input("enter something")
-
+# cv2.imwrite("/home/ggdhines/output.jpg",tess.training_page)
 tess.__update_tesseract__()
+
+#     # cur.execute("select * from characters where characters = \""+c+"\" and confidence < 80")
+#     # for r in cur.fetchall():
+#     #     subject_id,region,column,row,characters,confidence,lb_x,ub_x,lb_y,ub_y = r
+#     #
+#     #     if subject_id != current_subject:
+#     #         current_subject = subject_id
+#     #         fname = base + subject_id + ".JPG"
+#     #         image = cv2.imread(fname)
+#     #
+#     #         sub_image = image[region_bound[2]:region_bound[3],region_bound[0]:region_bound[1]]
+#     #
+#     #         gray = cv2.cvtColor(sub_image,cv2.COLOR_BGR2GRAY)
+#     #         # masked_image = preprocessing.__mask_lines__(gray)
+#     #
+#     #         height,width,_ = sub_image.shape
+#     #
+#     #     char_image = gray[lb_y:ub_y+1,lb_x:ub_x+1]
+#     #
+#     #     plt.imshow(char_image,cmap="gray")
+#     #     plt.title(characters + " -- " + str(confidence))
+#     #     plt.show()
+#     # continue
+#
+#     # cur.execute("select count(*) from characters where characters = \""+char+"\"")
+#     # a = cur.fetchone()
+#     # cur.execute("select count(*) from characters where characters = \""+char+"\" and confidence > 80")
+#     # b = cur.fetchone()
+#     # if (a[0] > 0) and (b[0] == 0):
+#     #     raw_input((char,a))
+#     # continue
+#
+#
+#     # cur.execute("select * from characters where characters = \""+char+"\" and confidence < 80 and confidence > 70")
+#     cur.execute("select * from characters where characters = \""+char+"\" order by confidence desc")
+#
+#     examples = []
+#     confidence_values = []
+#     height_values = []
+#     width_values = []
+#
+#     for r in cur.fetchall()[:150]:
+#         subject_id,region,column,row,characters,confidence,lb_x,ub_x,lb_y,ub_y = r
+#         # print(confidence)
+#         # if subject_id != current_subject:
+#         #     current_subject = subject_id
+#         #     fname = base + subject_id + ".JPG"
+#         #     image = cv2.imread(fname)
+#         #
+#         #     sub_image = image[region_bound[2]:region_bound[3],region_bound[0]:region_bound[1]]
+#         #
+#         #     gray = cv2.cvtColor(sub_image,cv2.COLOR_BGR2GRAY)
+#         #     masked_image = preprocessing.__mask_lines__(gray)
+#
+#         if subject_id != current_subject_id:
+#             masked_image = cv2.imread("/home/ggdhines/to_upload3/"+subject_id+".jpg",0)
+#             current_subject_id = subject_id
+#         height,width = masked_image.shape
+#
+#         char_image = masked_image[lb_y:ub_y+1,lb_x:ub_x+1]
+#
+#         # plt.imshow(char_image,cmap="gray")
+#         # plt.show()
+#         #
+#         # input_ = raw_input("is it correct?(y/n) ")
+#         # if input_ == "n":
+#         #     continue
+#         # assert input_ == "y"
+#
+#         chr_height = ub_y-lb_y+1
+#         chr_width = ub_x-lb_x+1
+#
+#         examples.append(char_image)
+#         confidence_values.append(confidence)
+#         height_values.append(chr_height)
+#         width_values.append(chr_width)
+#
+#     # raw_input("enter")
+#
+#     if confidence_values == []:
+#         continue
+#
+#     # combined = zip(confidence_values,examples,height_values,width_values)
+#     # combined.sort(key=lambda x:x[0],reverse=True)
+#     # confidence_values,examples,height_values,width_values = zip(*combined[:150])
+#
+#     if height_values != []:
+#         print("for character : " + char + " we have " + str(len(height_values)) + " samples")
+#
+#         desired_height = int(np.median(height_values))
+#         desired_width = int(np.median(width_values))
+#
+#         weights = boltzman_sampling(confidence_values)
+#
+#         rescaled_examples = []
+#         for c in examples:
+#             c2 = cv2.resize(c,(desired_height,desired_width))
+#             # plt.imshow(c2)
+#             # plt.show()
+#
+#             rescaled_examples.append(c2)
+#
+#         for i in range(1):
+#             num_samples = len(weights)
+#             samples = [weighted_choice(weights) for i in range(max(1,min(int(num_samples/2.),20)))]
+#
+#
+#             image_list = [rescaled_examples[j] for j in samples]
+#             s = image_list[0].shape
+#
+#             # flatten_list = [im.reshape((im.shape[0]*im.shape[1])) for im in image_list]
+#             # pca = PCA(n_components=1)
+#             # pca_images = pca.fit_transform(flatten_list)
+#             # print(sum(pca.explained_variance_ratio_))
+#
+#             average_image = np.median(image_list,axis=0)
+#
+#
+#             # pca_average = np.median(pca_images,axis=0)
+#             # new_average = pca.inverse_transform(pca_average)
+#             # average_image = new_average.reshape(s)
+#
+#             average_image = cv2.normalize(average_image,average_image,0,255,cv2.NORM_MINMAX)
+#             average_image = average_image.astype(np.uint8)
+#             # print(average_image)
+#
+#             # ret,close = cv2.threshold(average_image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+#
+#             # kernel = np.ones((10,10),np.uint8)
+#             # close = cv2.morphologyEx(close,cv2.MORPH_DILATE,kernel,iterations = 1)
+#
+#             tess.__add_char__(char,average_image)
+#
+#         # raw_input("enter something")
+#
+# tess.__update_tesseract__()
--- active_weather/tesseract_update.py
+++ active_weather/tesseract_update.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import os
 from subprocess import call
+import matplotlib.pyplot as plt
 __author__ = 'ggdhines'
 
 spacing = 35
@@ -23,6 +24,8 @@ class TesseractUpdate:
 
         self.box_count = 0
 
+        self.character_heights = []
+
     def __create_blank_page__(self):
         """
         create a blank page where we'll put labelled characters
@@ -32,8 +35,8 @@ class TesseractUpdate:
             f.write("")
 
         self.width = 2508
-        self.height = 200
-        # self.height = 4000
+        # self.height = 200
+        self.height = 4000
         self.training_page = np.zeros((self.height,self.width),dtype=np.uint8)
         self.training_page.fill(255)
 
@@ -48,64 +51,64 @@ class TesseractUpdate:
         self.box_file_entries = []
         self.used_height = spacing
 
-    def __next_image__(self):
-        """
-        add each of the saved characters into the row
-        :return:
-        """
-        # if self.row_bitmaps != []:
-        #     # self.__update_training_image__()
-        #     # self.__update_box_entries__()
-        #
-        #     # move onto the next row
-        #     row_height = max([b.shape[0] for b in self.row_bitmaps])
-        #
-        #
-        #     # self.__update_box_entries__()
-        #     # self.__box_file_flush__()
-        self.__update_training_image__(save_image=True)
-        call(["tesseract", "active_weather.basic.exp"+str(self.box_count)+".tiff", "active_weather.basic.exp"+str(self.box_count), "nobatch" ,"box.train"])
+    # def __next_image__(self):
+    #     """
+    #     add each of the saved characters into the row
+    #     :return:
+    #     """
+    #     # if self.row_bitmaps != []:
+    #     #     # self.__update_training_image__()
+    #     #     # self.__update_box_entries__()
+    #     #
+    #     #     # move onto the next row
+    #     #     row_height = max([b.shape[0] for b in self.row_bitmaps])
+    #     #
+    #     #
+    #     #     # self.__update_box_entries__()
+    #     #     # self.__box_file_flush__()
+    #     self.__update_training_image__(save_image=True)
+    #     call(["tesseract", "active_weather.basic.exp"+str(self.box_count)+".tiff", "active_weather.basic.exp"+str(self.box_count), "nobatch" ,"box.train"])
+    #
+    #     self.box_count += 1
+    #
+    #     self.row_bitmaps = []
+    #     self.row_characters = []
+    #     self.column_pointer = spacing
+    #
+    #     self.training_page = None
+    #     # self.row_pointer += spacing + row_height
 
-        self.box_count += 1
+    # def __get_image_height__(self):
+    #     """
+    #     we'll trim the training image to be not much bigger than the text so return the height
+    #     :return:
+    #     """
+    #     if self.row_bitmaps == []:
+    #         return self.row_pointer+spacing
+    #     else:
+    #         row_height = max([b.shape[0] for b in self.row_bitmaps])
+    #         return self.row_pointer+row_height+spacing
 
-        self.row_bitmaps = []
-        self.row_characters = []
-        self.column_pointer = spacing
-
-        self.training_page = None
-        # self.row_pointer += spacing + row_height
-
-    def __get_image_height__(self):
-        """
-        we'll trim the training image to be not much bigger than the text so return the height
-        :return:
-        """
-        if self.row_bitmaps == []:
-            return self.row_pointer+spacing
-        else:
-            row_height = max([b.shape[0] for b in self.row_bitmaps])
-            return self.row_pointer+row_height+spacing
-
-    def __update_training_image__(self,save_image=False):
-        """
-        put the bitmaps into the training image
-        :return:
-        """
-        column_pointer = spacing
-
-        for b in self.row_bitmaps:
-            assert isinstance(b,np.ndarray)
-            height,width = b.shape
-
-            # row first and then column
-            self.training_page[self.row_pointer:self.row_pointer+height,column_pointer:column_pointer+width] = b
-
-            column_pointer += width + spacing
-        # assert([b.shape for b in self.row_bitmaps] != [])
-        # print([b.shape for b in self.row_bitmaps])
-
-        if save_image:
-            cv2.imwrite("active_weather.basic.exp"+str(self.box_count)+".tiff",self.training_page)
+    # def __update_training_image__(self,save_image=False):
+    #     """
+    #     put the bitmaps into the training image
+    #     :return:
+    #     """
+    #     column_pointer = spacing
+    #
+    #     for b in self.row_bitmaps:
+    #         assert isinstance(b,np.ndarray)
+    #         height,width = b.shape
+    #
+    #         # row first and then column
+    #         self.training_page[self.row_pointer:self.row_pointer+height,column_pointer:column_pointer+width] = b
+    #
+    #         column_pointer += width + spacing
+    #     # assert([b.shape for b in self.row_bitmaps] != [])
+    #     # print([b.shape for b in self.row_bitmaps])
+    #
+    #     if save_image:
+    #         cv2.imwrite("active_weather.basic.exp"+str(self.box_count)+".tiff",self.training_page)
 
     # def __box_file_flush__(self):
     #     """
@@ -118,30 +121,60 @@ class TesseractUpdate:
     #             f.write(str(a)+" "+str(b)+" "+str(c)+" " + str(d) + " " + str(e) + " 0\n")
 
 
-    def __box_file_update__(self):
+    # def __box_file_update__(self):
+    #     """
+    #     update the list of box file entries
+    #     :return:
+    #     """
+    #     # print("updating")
+    #
+    #     # pruned_height = self.__get_image_height__()
+    #
+    #     # with open("/tmp/tessdata/active_weather.lobster.exp0.box","a") as f:
+    #     with open("active_weather.basic.exp"+str(self.box_count)+".box","a") as f:
+    #         # for char,b in zip(self.row_characters,self.row_bitmaps):
+    #             # calculate the coordinates for the box file
+    #         b = self.row_bitmaps[-1]
+    #         char = self.row_characters[-1]
+    #         height,width = b.shape
+    #
+    #         a,b,c,d,e = char,self.column_pointer,self.height-(self.row_pointer+height-1),self.column_pointer+width-1,self.height-self.row_pointer
+    #         # self.box_file_entries.append([char,column_pointer,self.height-(self.row_pointer+height-1),column_pointer+width-1,self.height-self.row_pointer])
+    #         f.write(str(a)+" "+str(b)+" "+str(c)+" " + str(d) + " " + str(e) + " 0\n")
+    #
+    #         self.column_pointer += width + spacing
+    #     # print("active_weather.basic.exp"+str(self.box_count)+".box")
+    #     # assert False
+
+    def __write_out_row__(self):
         """
-        update the list of box file entries
+        put the bitmaps into the training image
         :return:
         """
-        # print("updating")
+        column_pointer = spacing
 
-        # pruned_height = self.__get_image_height__()
+        row_height = np.max([b.shape[0] for b in self.row_bitmaps])
 
-        # with open("/tmp/tessdata/active_weather.lobster.exp0.box","a") as f:
         with open("active_weather.basic.exp"+str(self.box_count)+".box","a") as f:
-            # for char,b in zip(self.row_characters,self.row_bitmaps):
-                # calculate the coordinates for the box file
-            b = self.row_bitmaps[-1]
-            char = self.row_characters[-1]
-            height,width = b.shape
+            for char,b in zip(self.row_characters,self.row_bitmaps):
+                assert isinstance(b, np.ndarray)
+                height, width = b.shape
 
-            a,b,c,d,e = char,self.column_pointer,self.height-(self.row_pointer+height-1),self.column_pointer+width-1,self.height-self.row_pointer
-            # self.box_file_entries.append([char,column_pointer,self.height-(self.row_pointer+height-1),column_pointer+width-1,self.height-self.row_pointer])
-            f.write(str(a)+" "+str(b)+" "+str(c)+" " + str(d) + " " + str(e) + " 0\n")
+                # row first and then column
+                additional_height = row_height-height
 
-            self.column_pointer += width + spacing
-        # print("active_weather.basic.exp"+str(self.box_count)+".box")
-        # assert False
+                self.training_page[self.row_pointer+additional_height:self.row_pointer + height+additional_height, column_pointer:column_pointer + width] = b
+                a, b, c, d, e = char, column_pointer, self.height - (self.row_pointer + height + additional_height), column_pointer + width, self.height - (self.row_pointer+additional_height)
+                f.write(str(a) + " " + str(b) + " " + str(c+1) + " " + str(d-1) + " " + str(e) + " 0\n")
+
+                column_pointer += width + spacing
+
+
+        self.row_pointer += spacing + row_height
+        self.column_pointer = spacing
+
+        self.row_bitmaps = []
+        self.row_characters = []
 
     def __add_char__(self,character,bitmap):
         """
@@ -151,44 +184,46 @@ class TesseractUpdate:
         :param additional_y_offset:
         :return:
         """
-        if self.training_page is None:
-            self.__create_blank_page__()
+        # if self.training_page is None:
+        #     self.__create_blank_page__()
 
         char_height,char_width = bitmap.shape
 
         # do we have too many characters for this row?
         # if so - flush
-        # if (self.column_pointer+char_width) >= self.width-spacing:
-        #     self.__next_row()
+        if (self.column_pointer+char_width) >= self.width-spacing:
+            self.__write_out_row__()
+
+        # self.character_heights.append(bitmap.shape[0])
+
 
         self.row_bitmaps.append(bitmap)
         self.row_characters.append(character)
-        # self.column_pointer += char_width + spacing
+        self.column_pointer += char_width + spacing
 
-        self.__box_file_update__()
+        # self.__box_file_update__()
 
     def __update_tesseract__(self):
         """
         actually run the code to update Tesseract
         :return:
         """
-        # if self.row_bitmaps != []:
-        #     self.__update_training_image__(True)
-        #     self.__box_file_flush__()
-
-        # os.chdir('tessdata')
-        # call(["tesseract", "active_weather.basic.exp0.tiff", "active_weather.basic.exp0", "nobatch" ,"box.train"])
-        # raw_input("hello world")
-        call(["unicharset_extractor", "active_weather.basic.exp*.box"])
-        # raw_input("goodbye")
+        if self.row_bitmaps != []:
+            self.__write_out_row__()
+        cv2.imwrite("active_weather.basic.exp" + str(self.box_count) + ".tiff", self.training_page)
+        call(["tesseract", "active_weather.basic.exp0.tiff", "active_weather.basic.exp0", "nobatch", "box.train"])
 
         with open("font_properties","w") as f:
             f.write("basic 0 0 0 0 0\n")
 
-        os.system("shapeclustering -F font_properties -U unicharset active_weather.basic.exp*.tr")
+        call(["unicharset_extractor", "*.box"])
 
-        os.system("mftraining -F font_properties -U unicharset -O active_weather.unicharset active_weather.basic.exp*.tr")
-        os.system("cntraining active_weather.basic.exp*.tr")
+        assert False
+
+        os.system("shapeclustering -F font_properties -U unicharset active_weather.basic.exp0.tr")
+
+        os.system("mftraining -F font_properties -U unicharset -O active_weather.unicharset active_weather.basic.exp0.tr")
+        os.system("cntraining active_weather.basic.exp0.tr")
 
         os.system("mv inttemp active_weather.inttemp")
         os.system("mv normproto active_weather.normproto")
@@ -196,11 +231,11 @@ class TesseractUpdate:
         os.system("mv shapetable active_weather.shapetable")
         os.system("combine_tessdata active_weather.")
 
-        os.system("mv active_weather.basic.* /tmp/tessdata/")
-        os.system("mv active_weather.inttemp /tmp/tessdata/")
-        os.system("mv active_weather.normproto /tmp/tessdata/")
-        os.system("mv active_weather.pffmtable /tmp/tessdata/")
-        os.system("mv active_weather.shapetable /tmp/tessdata/")
-        os.system("mv active_weather.traineddata /tmp/tessdata/")
-        os.system("mv active_weather.unicharset /tmp/tessdata/")
-        os.system("mv font_properties /tmp/tessdata/")
+        # os.system("mv active_weather.basic.* /tmp/tessdata/")
+        # os.system("mv active_weather.inttemp /tmp/tessdata/")
+        # os.system("mv active_weather.normproto /tmp/tessdata/")
+        # os.system("mv active_weather.pffmtable /tmp/tessdata/")
+        # os.system("mv active_weather.shapetable /tmp/tessdata/")
+        # os.system("mv active_weather.traineddata /tmp/tessdata/")
+        # os.system("mv active_weather.unicharset /tmp/tessdata/")
+        # os.system("mv font_properties /tmp/tessdata/")
