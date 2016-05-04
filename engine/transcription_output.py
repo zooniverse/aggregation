6dbf9e4f0ba58f5bb7b58e69508de6fe3c777bdd
Greg Hines <greg@zooniverse.org>
2016-05-04 14:25:53 +0100

    getting folger and annotate to have the same structured output

--- engine/transcription_output.py
+++ engine/transcription_output.py
@@ -20,6 +20,9 @@ class TranscriptionOutput:
         self.workflow_id = self.project.workflows.keys()[0]
         self.metadata = self.project.__get_subject_metadata__(self.workflow_id)
 
+        self.tags = None
+        self.reverse_tags = None
+
     def __json_output__(self):
         aggregations_to_json = dict()
         print("creating json output ready")
@@ -37,157 +40,12 @@ class TranscriptionOutput:
         self.__tar_output__()
 
     @abstractmethod
-    def __subject_to_json__(self,subject_id,aggregation):
-        """
-        transform the aggregation results into a json output for just one subject
-        :param aggregation: the aggregation results for just one subject
-        :return:
-        """
-        return dict()
+    def __tokenize_individual_transcriptions__(self,cluster):
+        return None
 
-    def __tar_output__(self):
-        aws_tar = self.project.__get_aws_tar_name__()
-        print("saving json results")
-        with tarfile.open("/tmp/"+aws_tar,mode="w") as t:
-            t.add("/tmp/"+str(self.project.project_id)+".json")
-
-
-
-
-class ShakespearesWorldOutput(TranscriptionOutput):
-    def __init__(self,project):
-        TranscriptionOutput.__init__(self,project)
-
-    def __subject_to_json__(self,subject_id,aggregation):
-        """
-        transform the aggregation results into a json output for just one subject
-        :param aggregation:
-        :return:
-        """
-        subject_json = {"text":[],"individual transcriptions":[], "accuracy":[], "coordinates" : [],"users_per_line":[]}
-        subject_json["metadata"] = self.metadata[subject_id]
-        subject_json["zooniverse subject id"] = subject_id
-
-        clusters_by_line = {}
-
-
-        for key,cluster in aggregation["T2"]["text clusters"].items():
-            if key == "all_users":
-                continue
-            if isinstance(cluster,str):
-                warning("cluster is in string format for some reason")
-                cluster = json.loads(cluster)
-
-            try:
-                # for dev only since we may not have updated every transcription
-                if cluster["cluster members"] == []:
-                    continue
-            except TypeError:
-                warning(cluster)
-                warning()
-                raise
-
-            index = cluster["set index"]
-            # text_y_coord.append((cluster["center"][2],cluster["center"][-1]))
-
-            if index not in clusters_by_line:
-                clusters_by_line[index] = [cluster]
-            else:
-                clusters_by_line[index].append(cluster)
-
-        cluster_set_coordinates = {}
-
-        for set_index,cluster_set in clusters_by_line.items():
-            # clusters are based on purely horizontal lines so we don't need to take the
-            # average or anything like that.
-            # todo - figure out what to do with vertical lines, probably keep them completely separate
-            cluster_set_coordinates[set_index] = cluster_set[0]["center"][2]
-
-        sorted_sets = sorted(cluster_set_coordinates.items(), key = lambda x:x[1])
-
-        for set_index,_ in sorted_sets:
-            cluster_set = clusters_by_line[set_index]
-
-            # now on the (slightly off chance) that there are multiple clusters for this line, sort them
-            # by x coordinates
-            line = [(cluster["center"][0],cluster["center"][-1]) for cluster in cluster_set]
-            line.sort(key = lambda x:x[0])
-            _,text = zip(*line)
-
-            text = list(text)
-            # for combining the possible multiple clusters for this line into one
-            merged_line = ""
-            for t in text:
-                # think that storing in postgres converts from str to unicode
-                # for general display, we don't need ord(24) ie skipped characters
-                new_t = t.replace(chr(24),"")
-                merged_line += new_t
-
-            # we seem to occasionally get lines that are just skipped characters (i.e. the string
-            # if just chr(24)) - don't report these lines
-            if merged_line != "":
-                # is this the first line we've encountered for this subject?
-                # add in the line of text
-                subject_json["text"].append(merged_line)
-
-                # now add in the aligned individual transcriptions
-                # use the first cluster we found for this line as a "representative cluster"
-                rep_cluster = cluster_set[0]
-
-                zooniverse_ids = []
-                for user_id in rep_cluster["cluster members"]:
-                    zooniverse_login_name = self.project.__get_login_name__(user_id)
-
-                    # todo - not sure why None can be returned but does seem to happen
-                    if zooniverse_login_name is not None:
-                        # new_json[subject_id]["users_per_line"].append(zooniverse_login_name)
-                        zooniverse_ids.append(zooniverse_login_name)
-                    else:
-                        zooniverse_ids.append("None")
-
-                # todo - if a line is transcribed completely but in distinct separate parts
-                # todo - this may cause trouble
-                subject_json["individual transcriptions"].append(rep_cluster["aligned_text"])
-                subject_json["users_per_line"].append(zooniverse_ids)
-
-                # what was the accuracy for this line?
-                accuracy = len([c for c in merged_line if ord(c) != 27])/float(len(merged_line))
-                subject_json["accuracy"].append(accuracy)
-
-                # add in the coordinates
-                # this is only going to work with horizontal lines
-                line_segments = [cluster["center"][:-1] for cluster in cluster_set]
-                x1,x2,y1,y2 = zip(*line_segments)
-
-                # find the line segments which define the start and end of the line overall
-                x_start = min(x1)
-                x_end = max(x2)
-
-                start_index = np.argmin(x1)
-                end_index = np.argmax(x2)
-
-                y_start = y1[start_index]
-                y_end = y1[end_index]
-
-                subject_json["coordinates"].append([x_start,x_end,y_start,y_end])
-
-        return subject_json
-
-
-
-
-
-class AnnotateOutput(TranscriptionOutput):
-    def __init__(self,project):
-        TranscriptionOutput.__init__(self,project)
-
-        # set up the reverse tags - so we can take a token (representing a full tag) and replace it with the
-        # original tag
-        self.tags = self.project.text_algorithm.tags
-        self.reverse_tags = dict()
-
-        for a,b in self.tags.items():
-            self.reverse_tags[b] = a
+    @abstractmethod
+    def __generate_transcriptions_and_coordinates__(self,cluster):
+        yield None
 
     def __write_out_cluster__(self,cluster):
         """
@@ -200,24 +58,9 @@ class AnnotateOutput(TranscriptionOutput):
             cluster = cluster[0]
 
         cluster_to_json = {}
+
         # we need to retokenize everything
-
-        tokenized_strings = []
-        try:
-            for annotation in cluster["cluster members"]:
-                # extract the individual transcription
-                i_transcription = annotation[-2]
-                # convert from unicode to ascii
-                i_transcription = i_transcription.encode('ascii','ignore')
-                # replace each tag with a single character representation
-                for chr_representation,tag in self.tags.items():
-                    i_transcription = i_transcription.replace(tag,chr(chr_representation))
-
-                tokenized_strings.append(i_transcription)
-        except TypeError:
-            print(cluster)
-            print(type(cluster))
-            raise
+        tokenized_strings = self.__tokenize_individual_transcriptions__(cluster)
 
         # and now repeat with aggregated line
         aggregated_line = str(cluster["center"][-1])
@@ -241,10 +84,7 @@ class AnnotateOutput(TranscriptionOutput):
         :return:
         """
         individual_text_to_json = []
-        for ii, annotation in enumerate(cluster["cluster members"]):
-            # I think annotation[5] is no variants of words - for folger
-            coords = annotation[:4]
-            individual_text = annotation[4]
+        for coords,individual_text in self.__generate_transcriptions_and_coordinates__(cluster):
             assert isinstance(individual_text,unicode)
 
             # again, convert the tags to the ones needed by Folger or Tate (as opposed to the ones
@@ -421,3 +261,220 @@ class AnnotateOutput(TranscriptionOutput):
                     global_list.append({"coordinates":coords,"text":individual_text})
 
         return global_list
+
+    def __tar_output__(self):
+        aws_tar = self.project.__get_aws_tar_name__()
+        print("saving json results")
+        with tarfile.open("/tmp/"+aws_tar,mode="w") as t:
+            t.add("/tmp/"+str(self.project.project_id)+".json")
+
+
+class ShakespearesWorldOutput(TranscriptionOutput):
+    def __init__(self,project):
+        TranscriptionOutput.__init__(self,project)
+
+        self.tags = self.project.text_algorithm.tags
+        self.reverse_tags = dict()
+
+        # convert the tags into "folger safe" tags
+        for key,tag in self.tags.items():
+            self.tags[key] = tag.replace("sw-","")
+
+        for a,b in self.tags.items():
+            self.reverse_tags[b] = a
+
+    # def __subject_to_json__(self,subject_id,aggregation):
+    #     """
+    #     transform the aggregation results into a json output for just one subject
+    #     :param aggregation:
+    #     :return:
+    #     """
+    #     subject_json = {"text":[],"individual transcriptions":[], "accuracy":[], "coordinates" : [],"users_per_line":[]}
+    #     subject_json["metadata"] = self.metadata[subject_id]
+    #     subject_json["zooniverse subject id"] = subject_id
+    #
+    #     clusters_by_line = {}
+    #
+    #
+    #     for key,cluster in aggregation["T2"]["text clusters"].items():
+    #         if key == "all_users":
+    #             continue
+    #         if isinstance(cluster,str):
+    #             warning("cluster is in string format for some reason")
+    #             cluster = json.loads(cluster)
+    #
+    #         try:
+    #             # for dev only since we may not have updated every transcription
+    #             if cluster["cluster members"] == []:
+    #                 continue
+    #         except TypeError:
+    #             warning(cluster)
+    #             warning()
+    #             raise
+    #
+    #         index = cluster["set index"]
+    #         # text_y_coord.append((cluster["center"][2],cluster["center"][-1]))
+    #
+    #         if index not in clusters_by_line:
+    #             clusters_by_line[index] = [cluster]
+    #         else:
+    #             clusters_by_line[index].append(cluster)
+    #
+    #     cluster_set_coordinates = {}
+    #
+    #     for set_index,cluster_set in clusters_by_line.items():
+    #         # clusters are based on purely horizontal lines so we don't need to take the
+    #         # average or anything like that.
+    #         # todo - figure out what to do with vertical lines, probably keep them completely separate
+    #         cluster_set_coordinates[set_index] = cluster_set[0]["center"][2]
+    #
+    #     sorted_sets = sorted(cluster_set_coordinates.items(), key = lambda x:x[1])
+    #
+    #     for set_index,_ in sorted_sets:
+    #         cluster_set = clusters_by_line[set_index]
+    #
+    #         # now on the (slightly off chance) that there are multiple clusters for this line, sort them
+    #         # by x coordinates
+    #         line = [(cluster["center"][0],cluster["center"][-1]) for cluster in cluster_set]
+    #         line.sort(key = lambda x:x[0])
+    #         _,text = zip(*line)
+    #
+    #         text = list(text)
+    #         # for combining the possible multiple clusters for this line into one
+    #         merged_line = ""
+    #         for t in text:
+    #             # think that storing in postgres converts from str to unicode
+    #             # for general display, we don't need ord(24) ie skipped characters
+    #             new_t = t.replace(chr(24),"")
+    #             merged_line += new_t
+    #
+    #         # we seem to occasionally get lines that are just skipped characters (i.e. the string
+    #         # if just chr(24)) - don't report these lines
+    #         if merged_line != "":
+    #             # is this the first line we've encountered for this subject?
+    #             # add in the line of text
+    #             subject_json["text"].append(merged_line)
+    #
+    #             # now add in the aligned individual transcriptions
+    #             # use the first cluster we found for this line as a "representative cluster"
+    #             rep_cluster = cluster_set[0]
+    #
+    #             zooniverse_ids = []
+    #             for user_id in rep_cluster["cluster members"]:
+    #                 zooniverse_login_name = self.project.__get_login_name__(user_id)
+    #
+    #                 # todo - not sure why None can be returned but does seem to happen
+    #                 if zooniverse_login_name is not None:
+    #                     # new_json[subject_id]["users_per_line"].append(zooniverse_login_name)
+    #                     zooniverse_ids.append(zooniverse_login_name)
+    #                 else:
+    #                     zooniverse_ids.append("None")
+    #
+    #             # todo - if a line is transcribed completely but in distinct separate parts
+    #             # todo - this may cause trouble
+    #             subject_json["individual transcriptions"].append(rep_cluster["aligned_text"])
+    #             subject_json["users_per_line"].append(zooniverse_ids)
+    #
+    #             # what was the accuracy for this line?
+    #             accuracy = len([c for c in merged_line if ord(c) != 27])/float(len(merged_line))
+    #             subject_json["accuracy"].append(accuracy)
+    #
+    #             # add in the coordinates
+    #             # this is only going to work with horizontal lines
+    #             line_segments = [cluster["center"][:-1] for cluster in cluster_set]
+    #             x1,x2,y1,y2 = zip(*line_segments)
+    #
+    #             # find the line segments which define the start and end of the line overall
+    #             x_start = min(x1)
+    #             x_end = max(x2)
+    #
+    #             start_index = np.argmin(x1)
+    #             end_index = np.argmax(x2)
+    #
+    #             y_start = y1[start_index]
+    #             y_end = y1[end_index]
+    #
+    #             subject_json["coordinates"].append([x_start,x_end,y_start,y_end])
+    #
+    #     return subject_json
+
+
+    def __tokenize_individual_transcriptions__(self,cluster):
+        """
+        convert each individual string into a tokenized representation (so each tag is just one character)
+        :param cluster:
+        :return:
+        """
+        tokenized_strings = []
+        for text in cluster["aligned_text"]:
+            text = text.encode('ascii','ignore')
+            for chr_representation,tag in self.tags.items():
+                text = text.replace(tag,chr(chr_representation))
+
+            tokenized_strings.append(text)
+
+        return tokenized_strings
+
+    def __generate_transcriptions_and_coordinates__(self,cluster):
+        """
+        yield of all of the individual transcriptions and coordinates
+        :param cluster:
+        :return:
+        """
+        for coords,text in zip(cluster["individual points"],cluster["aligned_text"]):
+            yield coords,text
+
+        raise StopIteration()
+
+
+
+class AnnotateOutput(TranscriptionOutput):
+    def __init__(self,project):
+        TranscriptionOutput.__init__(self,project)
+
+        # set up the reverse tags - so we can take a token (representing a full tag) and replace it with the
+        # original tag
+        self.tags = self.project.text_algorithm.tags
+        self.reverse_tags = dict()
+
+        for a,b in self.tags.items():
+            self.reverse_tags[b] = a
+
+    # def __generate_individual_transcriptions__(self,cluster):
+    #     for annotation in cluster["cluster members"]:
+    #         yield annotation[-2]
+    #
+    #     raise StopIteration()
+    # try:
+    #         for annotation in cluster["cluster members"]:
+    #             # extract the individual transcription
+    #             try:
+    #                 i_transcription = annotation[-2]
+    #             except TypeError:
+    #                 print(cluster)
+    #                 print(annotation)
+    #                 raise
+    #             # convert from unicode to ascii
+    #             i_transcription = i_transcription.encode('ascii','ignore')
+    #             # replace each tag with a single character representation
+    #             for chr_representation,tag in self.tags.items():
+    #                 i_transcription = i_transcription.replace(tag,chr(chr_representation))
+    #
+    #             tokenized_strings.append(i_transcription)
+    #     except TypeError:
+    #         print(cluster)
+    #         print(type(cluster))
+    #         raise
+
+    # def __generate_transcriptions_and_coordinates__(self,cluster):
+    #     for ii, annotation in enumerate(cluster["cluster members"]):
+    #         # I think annotation[5] is no variants of words - for folger
+    #         coords = annotation[:4]
+    #         individual_text = annotation[4]
+    #
+    #         yield coords,individual_text
+    #
+    #     raise StopIteration()
+
+
+
