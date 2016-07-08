83e018fb504b0bf672f5c9c0d1ff0d71237bd728
Adam McMaster <adam@zooniverse.org>
2016-07-08 11:28:18 +0100

    Wrap long lines in __setup__

--- engine/aggregation_api.py
+++ engine/aggregation_api.py
@@ -162,36 +162,47 @@ class AggregationAPI:
         :return:
         """
         print("setting up")
-        # just for when we are treating an ouroboros project like a panoptes one
-        # in which the subject ids will be zooniverse_ids, which are strings
+        # just for when we are treating an ouroboros project like a panoptes
+        # one in which the subject ids will be zooniverse_ids, which are
+        # strings
         self.subject_id_type = "int"
 
-        # todo - some time in the far future - complete support for expert annotations
+        # todo - some time in the far future - complete support for expert
+        # annotations
         self.experts = []
 
         # load in the project id (a number)
         # if one isn't provided, tried reading it from the yaml config file
-        # todo - if we are not using a secure connection, this forces the project_id
-        # todo - to be provided as a param (since we don't read in the yaml file)
-        # todo - probably want to change that at some point as there is value in
-        # todo - non logged in users having a yaml file (where they can give csv classification files etc.)
+
+        # TODO: If we are not using a secure connection, this forces the
+        #       project_id to be provided as a param (since we don't read in
+        #       the yaml file) probably want to change that at some point as
+        #       there is value in non logged in users having a yaml file (where
+        #       they can give csv classification files etc.)
 
         #########
-        # everything that follows assumes you have a secure connection to Panoptes
-        # plus the DBs (either production or staging)
+        # everything that follows assumes you have a secure connection to
+        # Panoptes plus the DBs (either production or staging)
 
         param_file = open("/app/config/aggregation.yml","rb")
         param_details = yaml.load(param_file)
 
         environment_details = param_details[self.environment]
         # do we have a specific date as the minimum date for this project?
-        if (self.project_id in param_details) and ("default_date" in param_details[self.project_id]):
-            self.previous_runtime = parser.parse(param_details[self.project_id]["default_date"])
+        if (
+            (self.project_id in param_details) and
+            ("default_date" in param_details[self.project_id])
+        ):
+            self.previous_runtime = parser.parse(
+                param_details[self.project_id]["default_date"]
+            )
         # connect to the Cassandra DB
         # only if we have given the necessary param
         # and register this run
         if "cassandra" in environment_details:
-            self.cassandra_session = self.__cassandra_connect__(environment_details["cassandra"])
+            self.cassandra_session = self.__cassandra_connect__(
+                environment_details["cassandra"]
+            )
             self.__register_run__()
 
         # connect to whatever postgres db we want to
@@ -202,7 +213,10 @@ class AggregationAPI:
         self.classification_table = "classifications"
 
         # if we are using a public panoptes connection
-        if ("public_connection" in environment_details) and (environment_details["public_connection"] == True):
+        if (
+            ("public_connection" in environment_details) and
+            (environment_details["public_connection"] == True)
+        ):
             # go with the very basic connection
             print("trying public Panoptes connection - no login")
             self.host = "https://panoptes.zooniverse.org/"
@@ -215,10 +229,16 @@ class AggregationAPI:
 
         self.__get_project_details__()
 
-        # todo - refactor all this?
-        # there may be more than one workflow associated with a project - read them all in
-        # and set up the associated tasks
-        self.workflows,self.versions,self.instructions,self.updated_at_timestamps = self.__get_workflow_details__()
+        # TODO - refactor all this?
+        # there may be more than one workflow associated with a project - read
+        # them all in and set up the associated tasks
+        (
+            self.workflows,
+            self.versions,
+            self.instructions,
+            self.updated_at_timestamps
+        ) = self.__get_workflow_details__()
+
         self.retirement_thresholds = self.__get_retirement_threshold__()
         self.workflow_names = self.__get_workflow_names__()
 
@@ -231,7 +251,9 @@ class AggregationAPI:
                 print("aggregating only for workflow id : " + str(workflow_id))
                 self.workflows = {workflow_id: self.workflows[workflow_id]}
             except KeyError:
-                warning("did not have given desired workflow: " + str(workflow_id))
+                warning(
+                    "did not have given desired workflow: " + str(workflow_id)
+                )
                 warning("here's the workflows we do have")
                 warning(self.workflows)
                 raise
@@ -241,19 +263,21 @@ class AggregationAPI:
         # load the default classification algorithm
         self.__set_classification_alg__(classification.VoteCount)
 
-        # a bit of a sanity check in case I forget to change back up before uploading
-        # production and staging should ALWAYS pay attention to the version and only
+        # a bit of a sanity check in case I forget to change back up before
+        # uploading production and staging should ALWAYS pay attention to the
+        # version and only
         # aggregate retired subjects
         if self.environment in ["production","staging"]:
             self.only_retired_subjects = True
 
-        # bit of a stop gap measure - stores how many people have classified a given subject
+        # bit of a stop gap measure - stores how many people have classified a
+        # given subject
         self.classifications_per_subject = {}
 
         # do we want to aggregate over only retired subjects?
 
-        # do we want to aggregate over only subjects that have been retired/classified since
-        # the last time we ran the code?
+        # do we want to aggregate over only subjects that have been
+        # retired/classified since the last time we ran the code?
         self.only_recent_subjects = False
 
         self.oldest_new_classification = datetime.datetime.now()
