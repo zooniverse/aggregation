79bf485d0fd0ee326faa6dff6d208d38bcd19b35
Adam McMaster <adam@zooniverse.org>
2017-02-10 11:44:58 +0000

    Write env.sh to somewhere that won't get wiped...
    When other apps are deployed/when prod configs are updated.

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 [ -f /app/config/start.sh ] && sh /app/config/start.sh
 
-echo "export ENVIRONMENT=$ENVIRONMENT" > /app/config/env.sh
+echo "export ENVIRONMENT=$ENVIRONMENT" > /var/run/env.sh
 
 exec /usr/bin/supervisord -c  /etc/supervisor/supervisord.conf
